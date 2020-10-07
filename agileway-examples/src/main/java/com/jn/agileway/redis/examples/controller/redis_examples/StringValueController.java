3ba2cd30c79029ae7435707a1f94c82e8e186e6d
fangjinuo <fs1194361820@163.com>
2020-10-07 16:01:57 +0800

    * 更新测试用例

--- agileway-examples/src/main/java/com/jn/agileway/redis/examples/controller/redis_examples/StringValueController.java
+++ agileway-examples/src/main/java/com/jn/agileway/redis/examples/controller/redis_examples/StringValueController.java
@@ -22,7 +22,7 @@ public class StringValueController {
     private RedisConnectionFactory connectionFactory;
     private RedisLuaScriptRepository repository;
 
-    @Value("agileway.redis.global-key.prefix")
+    @Value("${agileway.redis.global-key.prefix}")
     private String keyPrefix;
 
     private RedisTemplate createRedisTemplate(TestScope scope) {
