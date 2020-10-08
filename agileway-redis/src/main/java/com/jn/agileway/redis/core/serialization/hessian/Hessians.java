6b2ba6b3985a0402fc3aeb539ac8751a5ad13385
fangjinuo <fs1194361820@163.com>
2020-10-08 15:31:32 +0800

    * 避免创建大量对象

--- agileway-redis/src/main/java/com/jn/agileway/redis/core/serialization/hessian/Hessians.java
+++ agileway-redis/src/main/java/com/jn/agileway/redis/core/serialization/hessian/Hessians.java
@@ -9,6 +9,22 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class Hessians {
+    private Hessians() {
+    }
+
+    private static ThreadLocal<Hessian2Output> outputThreadLocal = new ThreadLocal<Hessian2Output>() {
+        @Override
+        protected Hessian2Output initialValue() {
+            return new Hessian2Output();
+        }
+    };
+
+    private static ThreadLocal<Hessian2Input> inputThreadLocal = new ThreadLocal<Hessian2Input>() {
+        @Override
+        protected Hessian2Input initialValue() {
+            return new Hessian2Input();
+        }
+    };
 
     public static <T> byte[] serialize(T o) throws IOException {
         if (o == null) {
@@ -17,8 +33,9 @@ public class Hessians {
 
         Hessian2Output output = null;
         try {
+            output = outputThreadLocal.get();
             ByteArrayOutputStream bao = new ByteArrayOutputStream();
-            output = new Hessian2Output(bao);
+            output.init(bao);
             output.writeObject(o);
             return bao.toByteArray();
         } finally {
@@ -32,8 +49,9 @@ public class Hessians {
         }
         Hessian2Input input = null;
         try {
+            input = inputThreadLocal.get();
             ByteArrayInputStream bai = new ByteArrayInputStream(bytes);
-            input = new Hessian2Input(bai);
+            input.init(bai);
             return (T) input.readObject();
         } finally {
             IOs.close(input);
