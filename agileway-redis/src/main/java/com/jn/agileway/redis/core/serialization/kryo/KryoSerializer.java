cb428ad432e02ecfa9fe54ae542bb3bd6b86e4c9
fangjinuo <fs1194361820@163.com>
2020-10-08 16:22:44 +0800

    * 支持Kryo

--- agileway-redis/src/main/java/com/jn/agileway/redis/core/serialization/kryo/KryoSerializer.java
+++ agileway-redis/src/main/java/com/jn/agileway/redis/core/serialization/kryo/KryoSerializer.java
@@ -1,16 +1,15 @@
-package com.jn.agileway.redis.core.serialization.hessian;
+package com.jn.agileway.redis.core.serialization.kryo;
 
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
 import java.io.IOException;
 
-public class HessianSerializer<T> implements RedisSerializer<T> {
-
+public class KryoSerializer<T> implements RedisSerializer<T> {
     @Override
-    public byte[] serialize(T o) throws SerializationException {
+    public byte[] serialize(T t) throws SerializationException {
         try {
-            return Hessians.serialize(o);
+            return Kryos.serialize(t);
         } catch (IOException ex) {
             throw new SerializationException(ex.getMessage(), ex);
         }
@@ -19,14 +18,14 @@ public class HessianSerializer<T> implements RedisSerializer<T> {
     @Override
     public T deserialize(byte[] bytes) throws SerializationException {
         try {
-            return Hessians.<T>deserialize(bytes);
+            return Kryos.deserialize(bytes);
         } catch (IOException ex) {
             throw new SerializationException(ex.getMessage(), ex);
         }
     }
 
     @Override
-    public boolean canSerialize(Class type) {
+    public boolean canSerialize(Class<?> type) {
         return true;
     }
 
---
+++ agileway-redis/src/main/java/com/jn/agileway/redis/core/serialization/kryo/Kryos.java
@@ -0,0 +1,68 @@
+package com.jn.agileway.redis.core.serialization.kryo;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.jn.langx.factory.Factory;
+import com.jn.langx.factory.ThreadLocalFactory;
+import com.jn.langx.util.io.IOs;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class Kryos {
+    private Kryos() {
+    }
+
+    public static final ThreadLocalFactory<?, Kryo> kryoFactory = new ThreadLocalFactory<Object, Kryo>(new Factory<Object, Kryo>() {
+        @Override
+        public Kryo get(Object o) {
+            Kryo kryo = new Kryo();
+            kryo.setReferences(true);
+            return kryo;
+        }
+    });
+
+
+    public static <T> byte[] serialize(T o) throws IOException {
+        return serialize(kryoFactory, o);
+    }
+
+    public static <T> byte[] serialize(Factory<?, Kryo> kryoFactory, T o) throws IOException {
+        if (o == null) {
+            return null;
+        }
+
+        Output output = null;
+        try {
+            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+            output = new Output(bao);
+            kryoFactory.get(null).writeClassAndObject(output, o);
+            return bao.toByteArray();
+        } finally {
+            IOs.close(output);
+        }
+    }
+
+    public static <T> T deserialize(byte[] bytes) throws IOException {
+        return deserialize(kryoFactory, bytes);
+    }
+
+
+    public static <T> T deserialize(Factory<?, Kryo> kryoFactory, byte[] bytes) throws IOException {
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+        Input input = null;
+        try {
+
+            ByteArrayInputStream bai = new ByteArrayInputStream(bytes);
+            input = new Input(bai);
+            return (T) kryoFactory.get(null).readClassAndObject(input);
+        } finally {
+            IOs.close(input);
+        }
+    }
+
+}
