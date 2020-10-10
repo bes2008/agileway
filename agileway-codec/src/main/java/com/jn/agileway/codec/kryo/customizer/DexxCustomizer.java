246b80bbf5455194e717d4ef92bee85e8247b8db
fangjinuo <fs1194361820@163.com>
2020-10-10 16:31:15 +0800

    * 支持为 Dexx Android 添加 Kryo Customizer

---
+++ agileway-codec/src/main/java/com/jn/agileway/codec/kryo/customizer/DexxCustomizer.java
@@ -0,0 +1,24 @@
+package com.jn.agileway.codec.kryo.customizer;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.jn.agileway.codec.kryo.KryoCustomizer;
+import com.jn.langx.util.Platform;
+import de.javakaffee.kryoserializers.dexx.ListSerializer;
+import de.javakaffee.kryoserializers.dexx.MapSerializer;
+import de.javakaffee.kryoserializers.dexx.SetSerializer;
+
+public class DexxCustomizer implements KryoCustomizer {
+    @Override
+    public String getName() {
+        return "dexx";
+    }
+
+    @Override
+    public void customize(Kryo kryo) {
+        if (Platform.isAndroid) {
+            ListSerializer.registerSerializers(kryo);
+            MapSerializer.registerSerializers(kryo);
+            SetSerializer.registerSerializers(kryo);
+        }
+    }
+}
