32a6ef1cb278b63cadb573ec8abd9cfefd375d59
fs1194361820@163.com <fs1194361820@163.com>
2020-10-16 23:30:07 +0800

    * 支持 FSE 序列化框架

--- agileway-codec/src/main/java/com/jn/agileway/codec/fse/FseCodec.java
+++ agileway-codec/src/main/java/com/jn/agileway/codec/fse/FseCodec.java
@@ -1,16 +1,14 @@
 package com.jn.agileway.codec.fse;
 
-import com.jfireframework.fse.Fse;
 import com.jn.agileway.codec.AbstractCodec;
 import com.jn.agileway.codec.CodecException;
 
 public class FseCodec<T> extends AbstractCodec<T> {
-    private Fse fse = new Fse();
 
     @Override
     public byte[] encode(T obj) throws CodecException {
         try {
-            return Fses.serialize(fse, obj);
+            return Fses.serialize(obj);
         } catch (Throwable ex) {
             throw new CodecException(ex.getMessage(), ex);
         }
@@ -19,7 +17,7 @@ public class FseCodec<T> extends AbstractCodec<T> {
     @Override
     public T decode(byte[] bytes) throws CodecException {
         try {
-            return Fses.deserialize(fse, bytes);
+            return Fses.deserialize(bytes);
         } catch (Throwable ex) {
             throw new CodecException(ex.getMessage(), ex);
         }
@@ -28,7 +26,7 @@ public class FseCodec<T> extends AbstractCodec<T> {
     @Override
     public T decode(byte[] bytes, Class<T> targetType) throws CodecException {
         try {
-            return Fses.deserialize(fse, bytes, targetType);
+            return Fses.deserialize(bytes, targetType);
         } catch (Throwable ex) {
             throw new CodecException(ex.getMessage(), ex);
         }
