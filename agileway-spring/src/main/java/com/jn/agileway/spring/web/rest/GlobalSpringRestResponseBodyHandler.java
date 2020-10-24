b0ef8a709749bdf26a1d3fd845d2fb2269e382b6
fs1194361820@163.com <fs1194361820@163.com>
2020-10-24 20:30:07 +0800

    * 支持排除method

--- agileway-spring/src/main/java/com/jn/agileway/spring/web/rest/GlobalSpringRestResponseBodyHandler.java
+++ agileway-spring/src/main/java/com/jn/agileway/spring/web/rest/GlobalSpringRestResponseBodyHandler.java
@@ -51,7 +51,7 @@ public class GlobalSpringRestResponseBodyHandler implements GlobalRestResponseBo
     @Override
     public RestRespBody handleResponseBody(HttpServletRequest request, HttpServletResponse response, Method actionMethod, Object actionReturnValue) {
         if (!isSupportedAction(actionMethod)) {
-            request.setAttribute(GlobalRestHandlers.GLOBAL_REST_DISABLE_REST_REQUEST,true);
+            request.setAttribute(GlobalRestHandlers.GLOBAL_REST_NON_REST_REQUEST,true);
             return null;
         }
         if (actionReturnValue instanceof Resource) {
