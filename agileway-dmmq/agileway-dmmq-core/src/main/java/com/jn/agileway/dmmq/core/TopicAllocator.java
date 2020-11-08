c64e46147317091a6f67b9e73b567d1024722e56
fjn <1194361820@qq.com>
2020-11-08 15:46:19 +0800

    * 重构dmmq

--- agileway-dmmq/agileway-dmmq-core/src/main/java/com/jn/agileway/dmmq/core/TopicAllocator.java
+++ agileway-dmmq/agileway-dmmq-core/src/main/java/com/jn/agileway/dmmq/core/TopicAllocator.java
@@ -3,6 +3,10 @@ package com.jn.agileway.dmmq.core;
 import com.jn.agileway.dmmq.core.event.TopicEventListener;
 import com.jn.langx.util.function.Function;
 
+/**
+ * producer 使用它来根据 Message 计算出要分配的topic
+ * @param <M>
+ */
 public interface TopicAllocator<M> extends Function<M, String>, TopicEventListener {
 
 }
