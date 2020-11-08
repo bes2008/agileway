9ccf3c8e43893d7c0dcb4ae0c40863ffae474a40
fjn <1194361820@qq.com>
2020-11-08 16:32:08 +0800

    * 重构dmmq

--- agileway-dmmq/agileway-dmmq-core/src/main/java/com/jn/agileway/dmmq/core/consumer/DebugConsumer.java
+++ agileway-dmmq/agileway-dmmq-core/src/main/java/com/jn/agileway/dmmq/core/consumer/DebugConsumer.java
@@ -2,7 +2,6 @@ package com.jn.agileway.dmmq.core.consumer;
 
 import com.jn.agileway.dmmq.core.Consumer;
 import com.jn.agileway.dmmq.core.MessageHolder;
-import com.jn.langx.util.struct.Holder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,27 +14,27 @@ public class DebugConsumer<M> implements Consumer<M> {
     }
 
     @Override
-    public void onEvent(MessageHolder<M> event, long sequence, boolean endOfBatch) throws Exception {
-        logger.info("handling sequence:{}, endOfBatch:{}, message:{}", sequence, endOfBatch, event.get());
+    public void handleMessage(MessageHolder<M> messageHolder, long sequence, boolean endOfBatch) throws Exception {
+        logger.info("handling sequence:{}, endOfBatch:{}, message:{}", sequence, endOfBatch, messageHolder.get());
     }
 
     @Override
-    public void handleEventException(Throwable ex, long sequence, Object eventHolder) {
-        logger.warn("error when handle sequence: {}, message: {}, error:{} ", sequence, ((Holder<M>)eventHolder).get().toString(), ex);
+    public void handleMessageException(Throwable ex, long sequence, MessageHolder<M> messageHolder) {
+        logger.warn("error when handle sequence: {}, message: {}, error:{} ", sequence, messageHolder.get().toString(), ex);
     }
 
     @Override
-    public void handleOnStartException(Throwable ex) {
+    public void handleStartException(Throwable ex) {
         logger.error("error on start the DEBUG consumer, error message: {}, stack:{}", ex.getMessage(), ex);
     }
 
     @Override
-    public void handleOnShutdownException(Throwable ex) {
+    public void handleShutdownException(Throwable ex) {
         logger.error("error on shutdown the DEBUG consumer, error message: {}, stack:{}", ex.getMessage(), ex);
     }
 
     @Override
-    public void onTimeout(long sequence) throws Exception {
+    public void handleTimeout(long sequence) throws Exception {
         logger.warn("Timeout when handle sequence {} message ", sequence);
     }
 }
--- agileway-dmmq/agileway-dmmq-disruptor/src/main/java/com/jn/agileway/dmmq/disruptor/DisruptorConsumer.java
+++ agileway-dmmq/agileway-dmmq-disruptor/src/main/java/com/jn/agileway/dmmq/disruptor/DisruptorConsumer.java
@@ -6,5 +6,29 @@ import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.ExceptionHandler;
 import com.lmax.disruptor.TimeoutHandler;
 
-public interface DisruptorConsumer<M> extends Consumer<M>, EventHandler<MessageHolder<M>>, ExceptionHandler, TimeoutHandler {
+public abstract class DisruptorConsumer<M> implements Consumer<M>, EventHandler<MessageHolder<M>>, ExceptionHandler, TimeoutHandler {
+    @Override
+    public void onEvent(MessageHolder<M> event, long sequence, boolean endOfBatch) throws Exception {
+        handleMessage(event, sequence, endOfBatch);
+    }
+
+    @Override
+    public void onTimeout(long sequence) throws Exception {
+        handleTimeout(sequence);
+    }
+
+    @Override
+    public void handleEventException(Throwable ex, long sequence, Object event) {
+        handleMessageException(ex, sequence, (MessageHolder<M>) event);
+    }
+
+    @Override
+    public void handleOnStartException(Throwable ex) {
+        handleStartException(ex);
+    }
+
+    @Override
+    public void handleOnShutdownException(Throwable ex) {
+        handleShutdownException(ex);
+    }
 }
