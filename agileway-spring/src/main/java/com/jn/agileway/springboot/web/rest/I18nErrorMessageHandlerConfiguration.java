5b0d19a7a0b4fb55f75b5be13a697957d5cea9ad
fjn <1194361820@qq.com>
2020-11-23 19:43:04 +0800

    * 发布2.2.1版本

--- agileway-spring/src/main/java/com/jn/agileway/springboot/web/rest/I18nErrorMessageHandlerConfiguration.java
+++ agileway-spring/src/main/java/com/jn/agileway/springboot/web/rest/I18nErrorMessageHandlerConfiguration.java
@@ -2,6 +2,9 @@ package com.jn.agileway.springboot.web.rest;
 
 import com.jn.agileway.web.rest.I18nRestErrorMessageHandler;
 import com.jn.agileway.web.rest.I18nRestErrorMessageHandlerProperties;
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.text.i18n.AbstractResourceBundleI18nMessageStorage;
+import com.jn.langx.text.i18n.HierarchicalI18nMessageStorage;
 import com.jn.langx.text.i18n.I18nMessageStorage;
 import com.jn.langx.text.i18n.JdkResourceBundleI18nMessageStorage;
 import com.jn.langx.util.Strings;
@@ -32,10 +35,39 @@ public class I18nErrorMessageHandlerConfiguration {
         return storage;
     }
 
+    private I18nMessageStorage getRootI18nMessageStorage(@NonNull I18nMessageStorage storage) {
+        if (storage instanceof HierarchicalI18nMessageStorage) {
+            HierarchicalI18nMessageStorage s = (HierarchicalI18nMessageStorage) storage;
+            if (s.getParent() == null) {
+                return storage;
+            }
+            return getRootI18nMessageStorage(s.getParent());
+        } else {
+            return storage;
+        }
+
+    }
+
     @Bean
     @ConditionalOnMissingBean
     public I18nRestErrorMessageHandler i18nRestErrorMessageHandler(@Qualifier("globalI18nErrorMessageStorage") I18nMessageStorage storage, I18nRestErrorMessageHandlerProperties i18nRestErrorMessageHandlerProperties) {
         I18nRestErrorMessageHandler i18nRestErrorMessageHandler = new I18nRestErrorMessageHandler();
+
+        I18nMessageStorage root = getRootI18nMessageStorage(storage);
+        if (root != null) {
+            if (root instanceof AbstractResourceBundleI18nMessageStorage) {
+                String bundle = ((AbstractResourceBundleI18nMessageStorage) root).getBaseName();
+                if (!Strings.equalsIgnoreCase("agileway_error", bundle)) {
+                    JdkResourceBundleI18nMessageStorage agileway = new JdkResourceBundleI18nMessageStorage();
+                    agileway.setBasename("agileway_error");
+                    ((HierarchicalI18nMessageStorage) root).setParent(agileway);
+                }
+            } else if (root instanceof HierarchicalI18nMessageStorage) {
+                JdkResourceBundleI18nMessageStorage agileway = new JdkResourceBundleI18nMessageStorage();
+                agileway.setBasename("agileway_error");
+                ((HierarchicalI18nMessageStorage) root).setParent(agileway);
+            }
+        }
         i18nRestErrorMessageHandler.setI18nMessageStorage(storage);
         i18nRestErrorMessageHandler.setConfig(i18nRestErrorMessageHandlerProperties);
         return i18nRestErrorMessageHandler;
