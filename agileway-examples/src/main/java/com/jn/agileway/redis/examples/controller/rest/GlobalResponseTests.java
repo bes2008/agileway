e224a611be28b527e7b1b9c9ae7b99f4ffae5fee
fjn <1194361820@qq.com>
2020-12-09 16:46:10 +0800

    * 更新测试用例

--- agileway-examples/src/main/java/com/jn/agileway/redis/examples/controller/rest/GlobalResponseTests.java
+++ agileway-examples/src/main/java/com/jn/agileway/redis/examples/controller/rest/GlobalResponseTests.java
@@ -1,6 +1,8 @@
 package com.jn.agileway.redis.examples.controller.rest;
 
 import com.jn.agileway.redis.examples.controller.redis_examples.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/global_response")
 @RestController
 public class GlobalResponseTests {
-    @GetMapping
+
+    @GetMapping("/testBean")
     public Person aPerson(){
         Person p = new Person();
         p.setAge(10);
         p.setName("rest global response body");
         return p;
     }
+
+    @GetMapping("/testResponseEntityBean")
+    public ResponseEntity get(){
+        Person p = new Person();
+        p.setAge(10);
+        p.setName("rest global response body");
+
+        return new ResponseEntity<Person>( p, HttpStatus.OK);
+    }
+
 }
