2b332ed1b2f182813995508ca57f6d0dc84ca446
fjn <1194361820@qq.com>
2020-12-16 15:14:48 +0800

    - 迁移 agileway-jdbc-datasource模块

--- agileway-jdbc-datasource/src/main/java/com/jn/agileway/jdbc/ConnectionProxy.java
+++
@@ -1,9 +0,0 @@
-package com.jn.agileway.jdbc;
-
-import com.jn.langx.Delegatable;
-
-import java.sql.Connection;
-
-public interface ConnectionProxy extends Connection, Delegatable<Connection> {
-
-}
