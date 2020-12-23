5841d2627f5b77efe0d33d8e5efc9466bd45740a
fjn <1194361820@qq.com>
2020-12-23 11:27:25 +0800

    * 在Spring环境下提供基于 Expression的PointCut Advisor

---
+++ agileway-spring/src/main/java/com/jn/agileway/spring/aop/AspectjExpressionPointcutAdvisorBuilder.java
@@ -0,0 +1,43 @@
+package com.jn.agileway.spring.aop;
+
+import com.jn.langx.Builder;
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.util.Preconditions;
+import org.aopalliance.intercept.Interceptor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcutAdvisor;
+
+public class AspectjExpressionPointcutAdvisorBuilder implements Builder<AspectJExpressionPointcutAdvisor> {
+    private Interceptor interceptor;
+    private String expression;
+    private int order;
+
+    public AspectjExpressionPointcutAdvisorBuilder interceptor(@NonNull Interceptor interceptor) {
+        this.interceptor = interceptor;
+        return this;
+    }
+
+    public AspectjExpressionPointcutAdvisorBuilder expression(@NonNull String expression) {
+        this.expression = expression;
+        return this;
+    }
+
+    public AspectjExpressionPointcutAdvisorBuilder order(int order) {
+        this.order = order;
+        return this;
+    }
+
+
+    @Override
+    public AspectJExpressionPointcutAdvisor build() {
+        AspectJExpressionPointcutAdvisor advisor = new AspectJExpressionPointcutAdvisor();
+        Preconditions.checkNotNull(interceptor, "Error when build a aspectj expression pointcut advisor, the interceptor is null");
+        advisor.setAdvice(interceptor);
+
+        Preconditions.checkNotEmpty(expression, "Error when build a aspectj expression pointcut advisor, the expression is null or empty");
+        advisor.setExpression(expression);
+
+        advisor.setOrder(order);
+
+        return advisor;
+    }
+}
