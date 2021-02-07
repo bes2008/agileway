b3ed80c8637fa1120d181e9dcd306f80c30983f7
fjn <1194361820@qq.com>
2021-02-07 15:31:06 +0800

    * 完善sshj 访问

--- agileway-ssh/src/main/java/com/jn/agileway/ssh/client/impl/sshj/SshjConnection.java
+++ agileway-ssh/src/main/java/com/jn/agileway/ssh/client/impl/sshj/SshjConnection.java
@@ -4,15 +4,27 @@ import com.jn.agileway.ssh.client.AbstractSshConnection;
 import com.jn.agileway.ssh.client.SshException;
 import com.jn.agileway.ssh.client.channel.Channel;
 import com.jn.agileway.ssh.client.channel.SessionedChannel;
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.Strings;
+import com.jn.langx.util.collection.Collects;
 import com.jn.langx.util.net.Nets;
 import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.userauth.UserAuthException;
+import net.schmizz.sshj.userauth.keyprovider.KeyProvider;
+import net.schmizz.sshj.userauth.keyprovider.OpenSSHKeyFile;
+import net.schmizz.sshj.userauth.keyprovider.PKCS8KeyFile;
+import net.schmizz.sshj.userauth.keyprovider.PuTTYKeyFile;
+import net.schmizz.sshj.userauth.password.PasswordFinder;
+import net.schmizz.sshj.userauth.password.PasswordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.List;
 
 public class SshjConnection extends AbstractSshConnection<SshjConnectionConfig> {
     private Logger logger = LoggerFactory.getLogger(SshjConnection.class);
@@ -25,21 +37,21 @@ public class SshjConnection extends AbstractSshConnection<SshjConnectionConfig> 
     }
 
     @Override
-    public void connect(String host, int port)  throws SshException{
+    public void connect(String host, int port) throws SshException {
         try {
             connect(InetAddress.getByName(host), port);
-        }catch (UnknownHostException ex){
+        } catch (UnknownHostException ex) {
             throw new SshException(ex);
         }
     }
 
     @Override
-    public void connect(InetAddress host, int port)  throws SshException {
+    public void connect(InetAddress host, int port) throws SshException {
         connect(host, port, null, -1);
     }
 
     @Override
-    public void connect(InetAddress host, int port, InetAddress localAddr, int localPort)  throws SshException {
+    public void connect(InetAddress host, int port, InetAddress localAddr, int localPort) throws SshException {
         try {
             makeSureSshClient();
             if (!sshClient.isConnected()) {
@@ -49,13 +61,14 @@ public class SshjConnection extends AbstractSshConnection<SshjConnectionConfig> 
                     sshClient.connect(host, port, localAddr, localPort);
                 }
             }
-        }catch (Throwable ex){
+        } catch (Throwable ex) {
             throw new SshException(ex);
         }
     }
 
     @Override
     public boolean authenticateWithPassword(String user, String password) throws SshException {
+        Preconditions.checkNotNull(sshClient);
         try {
             sshClient.authPassword(user, password);
             return true;
@@ -68,14 +81,80 @@ public class SshjConnection extends AbstractSshConnection<SshjConnectionConfig> 
     }
 
     @Override
-    public boolean authenticateWithPublicKey(String user, char[] pemPrivateKey, String password) throws SshException {
+    public boolean authenticateWithPublicKey(String user, char[] pemPrivateKey, String passphrase) throws SshException {
+        Preconditions.checkNotNull(sshClient);
+
+        String keyContent = new String(pemPrivateKey);
+
+        List<KeyProvider> keyProviders = Collects.emptyArrayList();
+
+        PasswordFinder passwordFinder = null;
+        if (Strings.isNotBlank(passphrase)) {
+            passwordFinder = PasswordUtils.createOneOff(passphrase.toCharArray());
+        }
+        try {
+            StringReader reader = new StringReader(keyContent);
+            PuTTYKeyFile puTTYKeyFile = new PuTTYKeyFile();
+            if (passwordFinder == null) {
+                puTTYKeyFile.init(reader);
+            } else {
+                puTTYKeyFile.init(reader, passwordFinder);
+            }
+            puTTYKeyFile.getPrivate();
+            keyProviders.add(puTTYKeyFile);
+        } catch (Throwable ex) {
+            // ignore it
+        }
+
+        try {
+            StringReader reader = new StringReader(keyContent);
+            PKCS8KeyFile pkcs8KeyFile = new PKCS8KeyFile();
+            if (passwordFinder == null) {
+                pkcs8KeyFile.init(reader);
+            } else {
+                pkcs8KeyFile.init(reader, passwordFinder);
+            }
+            pkcs8KeyFile.getPrivate();
+            keyProviders.add(pkcs8KeyFile);
+        } catch (Throwable ex) {
+            // ignore it
+        }
+
+        try {
+            StringReader reader = new StringReader(keyContent);
+            OpenSSHKeyFile openSSHKeyFile = new OpenSSHKeyFile();
+            if (passwordFinder == null) {
+                openSSHKeyFile.init(reader);
+            } else {
+                openSSHKeyFile.init(reader, passwordFinder);
+            }
+            openSSHKeyFile.getPrivate();
+            keyProviders.add(openSSHKeyFile);
+        } catch (Throwable ex) {
+            // ignore it
+        }
+
+        try {
+            Preconditions.checkState(!keyProviders.isEmpty(), "the private key is invalid: " + keyContent);
+            sshClient.authPublickey(user, keyProviders);
+        } catch (UserAuthException ex) {
+            logger.error(ex.getMessage(), ex);
+            return false;
+        } catch (Throwable ex) {
+            throw new SshException(ex);
+        }
         return false;
     }
 
 
     @Override
     public SessionedChannel openSession() throws SshException {
-        return null;
+        try {
+            Session session = sshClient.startSession();
+            return new SshjSessionedChannel(session);
+        } catch (Throwable ex) {
+            throw new SshException(ex);
+        }
     }
 
     @Override
@@ -85,6 +164,6 @@ public class SshjConnection extends AbstractSshConnection<SshjConnectionConfig> 
 
     @Override
     public void close() throws IOException {
-
+        this.sshClient.close();
     }
 }
---
+++ agileway-ssh/src/main/java/com/jn/agileway/ssh/client/impl/sshj/SshjSessionedChannel.java
@@ -0,0 +1,184 @@
+package com.jn.agileway.ssh.client.impl.sshj;
+
+import com.jn.agileway.ssh.client.SshException;
+import com.jn.agileway.ssh.client.channel.SessionedChannel;
+import com.jn.agileway.ssh.client.utils.PTYMode;
+import com.jn.agileway.ssh.client.utils.Signal;
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.util.Preconditions;
+import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.enums.Enums;
+import com.jn.langx.util.function.Consumer2;
+import net.schmizz.sshj.connection.channel.direct.Session;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+class SshjSessionedChannel implements SessionedChannel {
+
+    private Session session;
+
+    private Session.Command command;
+    private Session.Subsystem subsystem;
+    private Session.Shell shell;
+
+    SshjSessionedChannel(@NonNull Session session) {
+        Preconditions.checkNotNull(session);
+        this.session = session;
+    }
+
+    @Override
+    public void pty(String term) throws IOException {
+        pty(term, 0, 0, 0, 0, null);
+    }
+
+    @Override
+    public void pty(String term, int termWidthCharacters, int termHeightCharacters, int termWidthPixels, int termHeightPixels, Map<PTYMode, Integer> terminalModes) throws IOException {
+        final Map<net.schmizz.sshj.connection.channel.direct.PTYMode, Integer> terminalModeMap = new HashMap<net.schmizz.sshj.connection.channel.direct.PTYMode, Integer>();
+        Collects.forEach(terminalModes, new Consumer2<PTYMode, Integer>() {
+            @Override
+            public void accept(PTYMode ptyMode, Integer value) {
+                String operateName = ptyMode.name();
+                net.schmizz.sshj.connection.channel.direct.PTYMode mode = Enums.ofName(net.schmizz.sshj.connection.channel.direct.PTYMode.class, operateName);
+                if (mode != null) {
+                    terminalModeMap.put(mode, value);
+                }
+            }
+        });
+        this.session.allocatePTY(term, termWidthCharacters, termHeightCharacters, termWidthPixels, termHeightPixels, terminalModeMap);
+    }
+
+    @Override
+    public void x11Forwarding(String hostname, int port, boolean singleConnection, String x11AuthenticationProtocol, String x11AuthenticationCookie, int x11ScreenNumber) throws IOException {
+        this.session.reqX11Forwarding(x11AuthenticationProtocol, x11AuthenticationCookie, x11ScreenNumber);
+    }
+
+    @Override
+    public void env(String variableName, String variableValue) throws SshException {
+        try {
+            this.session.setEnvVar(variableName, variableValue);
+        } catch (Throwable ex) {
+            throw new SshException(ex);
+        }
+    }
+
+    @Override
+    public void exec(String command) throws SshException {
+        Preconditions.checkNotEmpty(command, "the command is illegal : {}", command);
+        try {
+            this.command = this.session.exec(command);
+        } catch (Throwable ex) {
+            throw new SshException(ex);
+        }
+    }
+
+    @Override
+    public void subsystem(String subsystem) throws SshException {
+        Preconditions.checkNotEmpty(subsystem, "the subsystem is illegal : {}", subsystem);
+        try {
+            this.subsystem = this.session.startSubsystem(subsystem);
+        } catch (Throwable ex) {
+            throw new SshException(ex);
+        }
+    }
+
+    @Override
+    public void shell() throws SshException {
+        try {
+            this.shell = this.session.startShell();
+        } catch (Throwable ex) {
+            throw new SshException(ex);
+        }
+    }
+
+    @Override
+    public void signal(Signal signal) throws SshException {
+        Preconditions.checkNotEmpty(signal, "the signal is null or empty");
+        Preconditions.checkArgument(signal != Signal.UNKNOWN, "the signal is UNKNOWN");
+        net.schmizz.sshj.connection.channel.direct.Signal sg = null;
+        String name = signal.name();
+        sg = net.schmizz.sshj.connection.channel.direct.Signal.fromString(name);
+        try {
+            if (this.shell != null) {
+                this.shell.signal(sg);
+            }
+            if (this.command != null) {
+                this.command.signal(sg);
+            }
+        } catch (Throwable ex) {
+            throw new SshException(ex);
+        }
+    }
+
+    @Override
+    public String getType() {
+        if (shell != null) {
+            return "shell";
+        }
+        if (subsystem != null) {
+            return "subsystem";
+        }
+        if (command != null) {
+            return "exec";
+        }
+        return "session";
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.session.close();
+    }
+
+    @Override
+    public int getExitStatus() {
+        if (subsystem != null) {
+            return subsystem.getExitStatus();
+        }
+        if (command != null) {
+            return command.getExitStatus();
+        }
+        return 0;
+    }
+
+    @Override
+    public InputStream getErrorInputStream() throws SshException {
+        if (shell != null) {
+            return shell.getErrorStream();
+        }
+        if (command != null) {
+            return command.getErrorStream();
+        }
+        return null;
+    }
+
+    @Override
+    public InputStream getInputStream() throws SshException {
+        if (shell != null) {
+            return shell.getInputStream();
+        }
+        if (subsystem != null) {
+            return subsystem.getInputStream();
+        }
+        if (command != null) {
+            return command.getInputStream();
+        }
+        return null;
+    }
+
+    @Override
+    public OutputStream getOutputStream() throws SshException {
+        if (shell != null) {
+            return shell.getOutputStream();
+        }
+        if (subsystem != null) {
+            return subsystem.getOutputStream();
+        }
+        if (command != null) {
+            return command.getOutputStream();
+        }
+        return null;
+    }
+}
