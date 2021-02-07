7dad43bde5e799d70f7ff7c78ca6f542085903c1
fjn <1194361820@qq.com>
2021-02-07 19:18:24 +0800

    * 简化 known host verifier

--- agileway-ssh/src/main/java/com/jn/agileway/ssh/client/impl/ganymedssh2/Ssh2ConnectionFactory.java
+++ agileway-ssh/src/main/java/com/jn/agileway/ssh/client/impl/ganymedssh2/Ssh2ConnectionFactory.java
@@ -1,11 +1,10 @@
 package com.jn.agileway.ssh.client.impl.ganymedssh2;
 
-import ch.ethz.ssh2.KnownHosts;
 import com.jn.agileway.ssh.client.AbstractSshConnectionFactory;
 import com.jn.agileway.ssh.client.SshConnection;
+import com.jn.agileway.ssh.client.impl.ganymedssh2.verifier.FromSsh2HostKeyVerifierAdapter;
+import com.jn.agileway.ssh.client.impl.ganymedssh2.verifier.KnownHostsVerifier;
 import com.jn.agileway.ssh.client.utils.SshConfigs;
-import com.jn.langx.util.collection.Collects;
-import com.jn.langx.util.function.Consumer;
 
 import java.io.File;
 import java.util.List;
@@ -25,19 +24,8 @@ public class Ssh2ConnectionFactory extends AbstractSshConnectionFactory<Ssh2Conn
         List<File> paths = SshConfigs.getKnownHostsFiles(sshConfig.getKnownHostsPaths());
 
         if (!paths.isEmpty()) {
-            final KnownHosts knownHosts = new KnownHosts();
-            Collects.forEach(paths, new Consumer<File>() {
-                @Override
-                public void accept(File file) {
-                    try {
-                        knownHosts.addHostkeys(file);
-                    } catch (Throwable ex) {
-                        logger.error(ex.getMessage(), ex);
-                    }
-                }
-            });
-
-
+            KnownHostsVerifier verifier = new KnownHostsVerifier(paths);
+            connection.addHostKeyVerifier(new FromSsh2HostKeyVerifierAdapter(verifier));
         }
     }
 }
