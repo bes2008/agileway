d5525279de320f184d1f3d41d93e3d64202b89cf
fjn <1194361820@qq.com>
2021-02-26 18:10:52 +0800

    * reformat code

--- agileway-redis/src/main/java/com/jn/agileway/redis/core/script/lua/SetValueAndExpire.lua
+++ agileway-redis/src/main/java/com/jn/agileway/redis/core/script/lua/SetValueAndExpire.lua
@@ -1,7 +1,10 @@
 -- @return Object
 local value = redis.call('get', KEYS[1])
-    if value == false then redis.call('set', KEYS[1], ARGV[1]) redis.call('expire', KEYS[1], ARGV[2])
+if value == false then
+    redis.call('set', KEYS[1], ARGV[1])
+    redis.call('expire', KEYS[1], ARGV[2])
     return ARGV[1]
-    else redis.call('expire', KEYS[1], ARGV[2])
+else
+    redis.call('expire', KEYS[1], ARGV[2])
     return value
-    end
+end
