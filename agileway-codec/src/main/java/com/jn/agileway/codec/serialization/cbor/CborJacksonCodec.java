3a8de2fec0c20b0abc0d54533709f7ce25e9500e
fjn <1194361820@qq.com>
2021-03-04 15:46:43 +0800

    * 整理 Cbor实现

--- agileway-codec/src/main/java/com/jn/agileway/codec/serialization/cbor/CborJacksonCodec.java
+++ agileway-codec/src/main/java/com/jn/agileway/codec/serialization/cbor/CborJacksonCodec.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 import com.jn.agileway.codec.serialization.jackson.BasedJacksonCodec;
 
+/**
+ * 基于Jackson框架实现，由于Jackson框架不支持循环依赖，因而导致 Cbor 目前的实现，不支持循环依赖。
+ * @param <T>
+ */
 public class CborJacksonCodec<T> extends BasedJacksonCodec<T> {
 
     public CborJacksonCodec() {
