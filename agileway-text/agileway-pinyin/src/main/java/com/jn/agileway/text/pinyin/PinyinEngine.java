e0db5a128e3eacef17b32dd66ad437795fd7fa91
fjn <1194361820@qq.com>
2021-05-28 19:06:32 +0800

    * 改造拼音接口

--- agileway-text/agileway-pinyin/src/main/java/com/jn/agileway/text/pinyin/PinyinEngine.java
+++ agileway-text/agileway-pinyin/src/main/java/com/jn/agileway/text/pinyin/PinyinEngine.java
@@ -1,5 +1,7 @@
 package com.jn.agileway.text.pinyin;
 
+import java.util.List;
+
 /**
  * 拼音引擎接口，具体的拼音实现通过实现此接口，完成具体实现功能
  */
@@ -12,31 +14,22 @@ public interface PinyinEngine {
      * @param c 任意字符，汉字返回拼音，非汉字原样返回
      * @return 汉字返回拼音，非汉字原样返回
      */
-    String getPinyin(char c);
+    List<String> getPinyin(char c);
 
     /**
      * 获取字符串对应的完整拼音，非中文返回原字符
      *
      * @param str       字符串
-     * @param separator 拼音之间的分隔符
      * @return 拼音
      */
-    String getPinyin(String str, String separator);
+    List<String> getPinyin(String str);
 
-    /**
-     * 将输入字符串转为拼音首字母，其它字符原样返回
-     *
-     * @param c 任意字符，汉字返回拼音，非汉字原样返回
-     * @return 汉字返回拼音，非汉字原样返回
-     */
-    char getFirstLetter(char c);
 
-    /**
-     * 将输入字符串转为拼音首字母，其它字符原样返回
-     *
-     * @param str       任意字符，汉字返回拼音，非汉字原样返回
-     * @param separator 分隔符
-     * @return 汉字返回拼音，非汉字原样返回
-     */
-    String getFirstLetter(String str, String separator);
+    char toSimplifiedPinyin(char c);
+
+    String toSimplifiedPinyin(String str);
+
+    char toTraditionalPinyin(char c);
+
+    String toTraditionalPinyin(String str);
 }
