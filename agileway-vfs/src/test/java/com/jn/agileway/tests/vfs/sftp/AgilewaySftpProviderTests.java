41c02b42868ee00a39a7351133e4b04e9b2a6a4d
fjn <1194361820@qq.com>
2021-06-03 14:18:44 +0800

    * 启动VFSManager

--- agileway-vfs/src/test/java/com/jn/agileway/tests/vfs/sftp/AgilewaySftpProviderTests.java
+++ agileway-vfs/src/test/java/com/jn/agileway/tests/vfs/sftp/AgilewaySftpProviderTests.java
@@ -1,16 +1,10 @@
 package com.jn.agileway.tests.vfs.sftp;
 
-import com.jn.agileway.vfs.provider.VFSUtils;
-import com.jn.agileway.vfs.provider.sftp.SftpFileProvider;
-import org.apache.commons.vfs2.FileSystemManager;
-import org.apache.commons.vfs2.VFS;
-import org.apache.commons.vfs2.impl.DefaultFileSystemManager;
+import com.jn.agileway.vfs.provider.AgilewayVFSManagerBootstrap;
 
 public class AgilewaySftpProviderTests {
-    public static void main(String[] args) throws Throwable{
-        FileSystemManager defaultFSManager= VFS.getManager();
-        System.out.println(defaultFSManager.hasProvider("sftp"));
-        VFSUtils.addFileProvider((DefaultFileSystemManager)defaultFSManager,new SftpFileProvider(),"sftp");
+    public static void main(String[] args) throws Throwable {
+        AgilewayVFSManagerBootstrap.startup();
         System.out.println(1);
     }
 }
