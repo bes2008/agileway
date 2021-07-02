472d507537bcfd460e32072427e11750c5594c0d
fjn <1194361820@qq.com>
2021-07-02 12:12:35 +0800

    * 整理本地仓库路径

--- agileway-vfs/src/main/java/com/jn/agileway/vfs/artifact/repository/LocalArtifactRepositoryLayout.java
+++ agileway-vfs/src/main/java/com/jn/agileway/vfs/artifact/repository/LocalArtifactRepositoryLayout.java
@@ -1,11 +1,34 @@
 package com.jn.agileway.vfs.artifact.repository;
 
 import com.jn.agileway.vfs.artifact.Artifact;
+import com.jn.langx.util.Objs;
+import com.jn.langx.util.Strings;
 
 public class LocalArtifactRepositoryLayout implements ArtifactRepositoryLayout {
     @Override
     public String getPath(ArtifactRepository repository, Artifact artifact) {
-        return repository.getUrl() + repository.getBasedir() + artifact.getArtifactId() + artifact.getClassifier() + artifact.getExtension();
+        String path = repository.getUrl();
+        path = addSegment(path, repository.getBasedir());
+        path = addSegment(path, artifact.getArtifactId());
+        path = addSegment(path, artifact.getVersion());
+        path = addSegment(path, artifact.getArtifactId() + "-" + artifact.getVersion() + (Objs.isEmpty(artifact.getClassifier()) ? "" : ("." + artifact.getClassifier())) + "." + artifact.getExtension());
+        return path;
+    }
+
+    private String addSegment(String path, String segment) {
+
+        while (Strings.endsWith(segment, "/")) {
+            segment = segment.substring(0, segment.length() - 1);
+        }
+
+        while (Strings.startsWith(segment, "/")) {
+            segment = segment.substring(1);
+        }
+
+        while (Strings.endsWith(path, "/")) {
+            path = path.substring(0, path.length() - 1);
+        }
+        return path + "/" + segment;
     }
 
     @Override
