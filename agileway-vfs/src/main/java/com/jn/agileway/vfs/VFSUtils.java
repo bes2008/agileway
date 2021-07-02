c833a6c4a6ca0dcad19b81752c61ff55bb205679
fjn <1194361820@qq.com>
2021-07-02 09:41:28 +0800

    * 整理异常

--- agileway-vfs/src/main/java/com/jn/agileway/vfs/VFSUtils.java
+++ agileway-vfs/src/main/java/com/jn/agileway/vfs/VFSUtils.java
@@ -71,7 +71,7 @@ public class VFSUtils {
         try {
             return fileObject.exists();
         }catch (FileSystemException fileSystemException){
-            throw new VfsException();
+            throw new VfsException(fileSystemException);
         }
     }
 }
