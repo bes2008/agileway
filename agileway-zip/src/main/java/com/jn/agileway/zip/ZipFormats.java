9887f38aecb0cc7f63c759e481d9d1ef95a7ad26
fjn <1194361820@qq.com>
2021-07-04 09:43:16 +0800

    * 支持zipformats.yml解析

--- agileway-zip/src/main/java/com/jn/agileway/zip/ZipFormats.java
+++ agileway-zip/src/main/java/com/jn/agileway/zip/ZipFormats.java
@@ -1,8 +1,67 @@
 package com.jn.agileway.zip;
 
-public class ZipFormats {
-    private ZipFormats(){}
+import com.jn.langx.io.resource.Resource;
+import com.jn.langx.io.resource.Resources;
+import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.function.Consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.InputStream;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ZipFormats {
+    private static final Logger logger = LoggerFactory.getLogger(ZipFormats.class);
+
+    private ZipFormats() {
+    }
+
+    /**
+     * key: ZipFormat#getFormat()
+     */
+    private static Map<String, ZipFormat> registry = new ConcurrentHashMap<String, ZipFormat>();
+
+    static {
+
+    }
+
+    private static void loadBuiltinZipFormats() {
+        Resource resource = Resources.loadClassPathResource("/zipformats.yml");
+        InputStream stream = null;
+        try {
+            stream = resource.getInputStream();
+            Iterable<Object> objs = new Yaml().loadAll(stream);
+            Collects.forEach(objs, new Consumer<Object>() {
+                @Override
+                public void accept(Object o) {
+                    Map<String, String> map = (Map<String, String>) o;
+                    ZipFormat zipFormat = new ZipFormat();
+                    zipFormat.setFormat(map.get("format"));
+                    zipFormat.setArchive(map.get("archive"));
+                    zipFormat.setDesc(map.get("desc"));
+                    zipFormat.setUncompressSuffix(map.get("uncompressSuffix"));
+                    zipFormat.setCompress(map.get("compress"));
+                }
+            });
+        } catch (Throwable ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
+
+    public static void addZipFormat(ZipFormat format) {
+        registry.put(format.getFormat(), format);
+    }
+
+    public static ZipFormat getZipFormat(String format) {
+        return registry.get(format);
+    }
+
+    public static boolean archiveEnabled(String format) {
+        ZipFormat zf = getZipFormat(format);
+        return zf.archiveEnabled();
+    }
 
 
 }
