c10a140e38e2e585a01b712b9343025b9c29dc3e
fjn <1194361820@qq.com>
2021-07-14 18:54:17 +0800

    * 整理构建过程

--- agileway-shiro-redis-springdata2/build/clean.bat
+++ agileway-shiro-redis-springdata2/build/clean.bat
@@ -1,11 +1,6 @@
 echo current directory: %~dp0..
 
-set PROJECT_DIR=%~dp0..
-
-echo remove directory: %PROJECT_DIR%/agileway-redis-springdata2/src
-rmdir /S/Q "%PROJECT_DIR%/agileway-redis-springdata2/target"
-rmdir /S/Q "%PROJECT_DIR%/agileway-redis-springdata2/src"
-
+set PROJECT_DIR=%~dp0../..
 
 echo remove directory: %PROJECT_DIR%/agileway-shiro-redis-springdata2/src
 rmdir /S/Q "%PROJECT_DIR%/agileway-shiro-redis-springdata2/target"
