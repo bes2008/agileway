a4262786a842eccd4f34949b27e97d027795a3f5
fjn <fs1194361820@163.com>
2021-09-10 10:04:15 +0800

    Revert "* 更新代码"
    This reverts commit 6db8f143a13f7679e364b36c8dd4cfb7c682b5b1.

--- agileway-feign/src/main/java/com/jn/agileway/feign/supports/adaptable/AdaptableDecoder.java
+++ agileway-feign/src/main/java/com/jn/agileway/feign/supports/adaptable/AdaptableDecoder.java
@@ -47,6 +47,10 @@ public class AdaptableDecoder implements Decoder {
 
         Object result = delegate.decode(repeatableResponse, expected);
         result = adapter.adapt(repeatableResponse, expected, result);
+
+        if (repeatableResponse != response) {
+            IOs.close(response);
+        }
         return result;
     }
 
