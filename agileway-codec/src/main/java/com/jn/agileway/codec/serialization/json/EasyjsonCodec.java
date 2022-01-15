9ae036aaede749151c46ac6c55b668f7c2b2d18e
fjn <1194361820@qq.com>
2022-01-15 15:39:19 +0800

    * 整理codec

--- agileway-codec/src/main/java/com/jn/agileway/codec/serialization/json/EasyjsonCodec.java
+++ agileway-codec/src/main/java/com/jn/agileway/codec/serialization/json/EasyjsonCodec.java
@@ -1,6 +1,7 @@
 package com.jn.agileway.codec.serialization.json;
 
 import com.jn.agileway.codec.AbstractCodec;
+import com.jn.agileway.codec.serialization.SchemaedStruct;
 import com.jn.easyjson.core.JSONFactory;
 import com.jn.easyjson.core.factory.JsonFactorys;
 import com.jn.easyjson.core.factory.JsonScope;
@@ -8,7 +9,6 @@ import com.jn.langx.codec.CodecException;
 import com.jn.langx.util.ClassLoaders;
 import com.jn.langx.util.io.Charsets;
 import com.jn.langx.util.reflect.Reflects;
-import com.jn.langx.util.reflect.type.Primitives;
 
 public class EasyjsonCodec<T> extends AbstractCodec<T> {
     private JSONFactory jsonFactory = JsonFactorys.getJSONFactory(JsonScope.SINGLETON);
@@ -22,39 +22,39 @@ public class EasyjsonCodec<T> extends AbstractCodec<T> {
 
     @Override
     protected byte[] doEncode(T t, boolean withSchema) throws CodecException {
-        String json = jsonFactory.get().toJson(t);
-        if (withSchema) {
-            json = Reflects.getFQNClassName(Primitives.wrap(t.getClass())) + ";" + json;
-        }
-        return json.getBytes(Charsets.UTF_8);
+        return withSchema ? serializeWithSchema(t) : serialize(t);
     }
 
     @Override
     protected T doDecode(byte[] bytes, boolean withSchema, Class<T> targetType) throws CodecException {
-        String json = new String(bytes, Charsets.UTF_8);
-        Class<T> javaType = targetType;
-        if (javaType == null) {
-            javaType = getTargetType();
-        }
-        if (withSchema) {
-            int index = json.indexOf(";");
+        return withSchema ? (T) deserializeWithSchema(bytes) : deserialize(bytes, targetType);
+    }
 
-            if (index > 0) {
-                String classFQN = json.substring(0, index);
-                try {
-                    javaType = ClassLoaders.loadClass(classFQN);
-                } catch (ClassNotFoundException ex) {
-                    throw new CodecException(ex.getMessage());
-                }
-                json = json.substring(index + 1);
-            }
+    private byte[] serializeWithSchema(T obj) {
+        byte[] json = serialize(obj);
+        SchemaedStruct struct = new SchemaedStruct();
+        struct.setName(Reflects.getFQNClassName(obj.getClass()));
+        struct.setValue(json);
+        return serialize(struct);
+    }
 
+    private <E> byte[] serialize(E obj) {
+        String json = jsonFactory.get().toJson(obj);
+        return json.getBytes(Charsets.UTF_8);
+    }
+
+    private <E> E deserializeWithSchema(byte[] bytes) {
+        try {
+            SchemaedStruct struct = deserialize(bytes, SchemaedStruct.class);
+            Class<E> targetClass = ClassLoaders.loadClass(struct.getName());
+            return deserialize(struct.getValue(), targetClass);
+        } catch (Throwable ex) {
+            throw new CodecException(ex);
         }
-        if (javaType == Object.class) {
-            return (T) json;
-        } else {
-            return jsonFactory.get().fromJson(json, javaType);
-        }
+    }
+
+    private <E> E deserialize(byte[] bytes, Class<E> targetType) {
+        return jsonFactory.get().fromJson(new String(bytes, Charsets.UTF_8), targetType);
     }
 
     public JSONFactory getJsonFactory() {
