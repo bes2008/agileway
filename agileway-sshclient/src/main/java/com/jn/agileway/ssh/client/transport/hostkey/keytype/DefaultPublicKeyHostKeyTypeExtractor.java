1f60ad2cbe173b759cbed3cf896ea8f3ecee7089
fjn <fjn@163.com>
2022-03-24 17:12:05 +0800

    * 自动初始化 内置的 host key type extractor

--- agileway-sshclient/src/main/java/com/jn/agileway/ssh/client/transport/hostkey/keytype/DefaultPublicKeyHostKeyTypeExtractor.java
+++ agileway-sshclient/src/main/java/com/jn/agileway/ssh/client/transport/hostkey/keytype/DefaultPublicKeyHostKeyTypeExtractor.java
@@ -1,6 +1,9 @@
 package com.jn.agileway.ssh.client.transport.hostkey.keytype;
 
+import com.jn.agileway.ssh.client.transport.hostkey.codec.PublicKeyCodecRegistry;
 import com.jn.langx.annotation.Singleton;
+import com.jn.langx.lifecycle.AbstractInitializable;
+import com.jn.langx.lifecycle.InitializationException;
 import com.jn.langx.util.collection.Collects;
 import com.jn.langx.util.function.Consumer;
 import com.jn.langx.util.function.Predicate;
@@ -11,20 +14,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Singleton
-public class DefaultPublicKeyHostKeyTypeExtractor implements PublicKeyHostKeyTypeExtractor {
+public class DefaultPublicKeyHostKeyTypeExtractor extends AbstractInitializable implements PublicKeyHostKeyTypeExtractor {
     private static final DefaultPublicKeyHostKeyTypeExtractor INSTANCE = new DefaultPublicKeyHostKeyTypeExtractor();
     private Map<String, PublicKeyHostKeyTypeExtractor> extractorMap = new HashMap<String, PublicKeyHostKeyTypeExtractor>();
 
     private DefaultPublicKeyHostKeyTypeExtractor() {
+        init();
+    }
 
+    /**
+     * @since 2.7.11
+     */
+    @Override
+    protected void doInit(){
+        PublicKeyCodecRegistry.getInstance();
     }
 
     public static DefaultPublicKeyHostKeyTypeExtractor getInstance() {
         return INSTANCE;
     }
 
-    public void addPublicKeyHostKeyTypeExtractor(String name, PublicKeyHostKeyTypeExtractor extractor){
-        extractorMap.put(name,extractor);
+    public void addPublicKeyHostKeyTypeExtractor(String name, PublicKeyHostKeyTypeExtractor extractor) {
+        extractorMap.put(name, extractor);
     }
 
     @Override
--- agileway-sshclient/src/main/java/com/jn/agileway/ssh/client/transport/hostkey/keytype/KeyBytesHostKeyTypeExtractor.java
+++ agileway-sshclient/src/main/java/com/jn/agileway/ssh/client/transport/hostkey/keytype/KeyBytesHostKeyTypeExtractor.java
@@ -3,8 +3,11 @@ package com.jn.agileway.ssh.client.transport.hostkey.keytype;
 import com.jn.agileway.ssh.client.utils.Buffer;
 
 public class KeyBytesHostKeyTypeExtractor implements HostKeyTypeExtractor<byte[]> {
-    public static final KeyBytesHostKeyTypeExtractor INSTANCE=new KeyBytesHostKeyTypeExtractor();
-    private KeyBytesHostKeyTypeExtractor(){}
+    public static final KeyBytesHostKeyTypeExtractor INSTANCE = new KeyBytesHostKeyTypeExtractor();
+
+    private KeyBytesHostKeyTypeExtractor() {
+    }
+
     @Override
     public String get(byte[] publicKey) {
         return KeyBufferHostKeyTypeExtractor.INSTANCE.get(new Buffer.PlainBuffer(publicKey));
