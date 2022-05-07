34a3211d6fa07060f4951d27b20b82d4364a2703
fjn <fjn@163.com>
2022-05-07 18:58:43 +0800

    * 模块重命名

---
+++ agileway-jersey/src/main/java/com/jn/agileway/ws/rs/WsrsHttpRequest.java
@@ -0,0 +1,65 @@
+package com.jn.agileway.ws.rs;
+
+import com.jn.agileway.http.rr.HttpRequest;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import java.util.Enumeration;
+import java.util.Locale;
+
+public class WsrsHttpRequest implements HttpRequest<ContainerRequestContext> {
+    private ContainerRequestContext delegate;
+    @Override
+    public ContainerRequestContext getDelegate() {
+        return this.delegate;
+    }
+
+    @Override
+    public String getRemoteHost() {
+        return null;
+    }
+
+    @Override
+    public String getMethod() {
+        return null;
+    }
+
+    @Override
+    public String getRequestURI() {
+        return null;
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return null;
+    }
+
+    @Override
+    public Enumeration<String> getHeaderNames() {
+        return null;
+    }
+
+    @Override
+    public Enumeration<String> getHeaders(String name) {
+        return null;
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        return null;
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+    @Override
+    public StringBuffer getRequestURL() {
+        return null;
+    }
+}
---
+++ agileway-jersey/src/main/java/com/jn/agileway/ws/rs/WsrsHttpResponse.java
@@ -0,0 +1,39 @@
+package com.jn.agileway.ws.rs;
+
+import com.jn.agileway.http.rr.HttpResponse;
+
+import javax.ws.rs.container.ContainerResponseContext;
+import java.util.Collection;
+
+public class WsrsHttpResponse implements HttpResponse<ContainerResponseContext> {
+    private ContainerResponseContext delegate;
+
+    public ContainerResponseContext getDelegate(){
+        return this.delegate;
+    }
+
+    @Override
+    public void addHeader(String name, String value) {
+
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaderNames() {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaders(String name) {
+        return null;
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 0;
+    }
+}
