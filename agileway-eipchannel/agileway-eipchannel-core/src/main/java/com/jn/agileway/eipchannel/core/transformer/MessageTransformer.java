84cea92699c68fe352f0ac616052e227f5ba3ae1
fjn <fjn@163.com>
2022-09-02 14:17:17 +0800

    * 支持transformer

--- agileway-eipchannel/agileway-eipchannel-core/src/main/java/com/jn/agileway/eipchannel/core/transformer/MessageTransformer.java
+++ agileway-eipchannel/agileway-eipchannel-core/src/main/java/com/jn/agileway/eipchannel/core/transformer/MessageTransformer.java
@@ -1,7 +1,8 @@
 package com.jn.agileway.eipchannel.core.transformer;
 
 import com.jn.agileway.eipchannel.core.message.Message;
+import com.jn.langx.Transformer;
 
-public interface MessageTransformer {
+public interface MessageTransformer extends Transformer<Message<?>, Message<?>> {
     Message<?> transform(Message<?> message);
 }
