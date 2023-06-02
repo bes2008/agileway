d97bdfbc8786ad37789e8c37685880bbfc94b60f
fjn1194361820 <fs1194361820@163.com>
2023-06-02 10:39:10 +0800

    * 整理ssh模块

---
+++ agileway-ssh/agileway-sshclient-tests/src/test/java/com/jn/agileway/ssh/test/channel/direct/session/command/SshClients_CommandLineTests.java
@@ -0,0 +1,94 @@
+package com.jn.agileway.ssh.test.channel.direct.session.command;
+
+import com.jn.agileway.ssh.client.*;
+import com.jn.agileway.ssh.client.impl.ganymedssh2.Ssh2ConnectionConfig;
+import com.jn.agileway.ssh.client.impl.ganymedssh2.Ssh2ConnectionFactory;
+import com.jn.agileway.ssh.client.impl.j2ssh.J2sshConnectionConfig;
+import com.jn.agileway.ssh.client.impl.j2ssh.J2sshConnectionFactory;
+import com.jn.agileway.ssh.client.impl.jsch.JschConnectionConfig;
+import com.jn.agileway.ssh.client.impl.jsch.JschConnectionFactory;
+import com.jn.agileway.ssh.client.impl.sshj.SshjConnectionConfig;
+import com.jn.agileway.ssh.client.impl.sshj.SshjConnectionFactory;
+import com.jn.agileway.ssh.client.impl.synergy.SynergyConnectionConfig;
+import com.jn.agileway.ssh.client.impl.synergy.SynergyConnectionFactory;
+import com.jn.agileway.ssh.client.supports.command.SshCommandResponse;
+import com.jn.langx.util.logging.Loggers;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+
+public class SshClients_CommandLineTests {
+    private static Logger logger = Loggers.getLogger(SshClients_CommandLineTests.class);
+
+
+    @Test
+    public void testJsch() throws Throwable {
+        JschConnectionConfig connectionConfig = new JschConnectionConfig();
+        testExec(new JschConnectionFactory(), connectionConfig);
+    }
+
+
+    @Test
+    public void testGanymedSsh2() throws Throwable {
+        testExec(new Ssh2ConnectionFactory(), new Ssh2ConnectionConfig());
+    }
+
+
+    @Test
+    public void testTrileadSsh2() throws Throwable {
+        testExec(new com.jn.agileway.ssh.client.impl.trileadssh2.Ssh2ConnectionFactory(), new com.jn.agileway.ssh.client.impl.trileadssh2.Ssh2ConnectionConfig());
+    }
+
+    @Test
+    public void testSshj() throws Throwable {
+        testExec(new SshjConnectionFactory(), new SshjConnectionConfig());
+    }
+
+    @Test
+    public void testSynergy() throws Throwable {
+        testExec(new SynergyConnectionFactory(), new SynergyConnectionConfig());
+    }
+
+    /**
+     * J2ssh 问题较多，目前无法进行测试
+     * @throws Throwable
+     */
+    //@Test
+    public void testJ2ssh() throws Throwable {
+        testExec(new J2sshConnectionFactory(), new J2sshConnectionConfig());
+    }
+
+
+    private void testExec(SshConnectionFactory connectionFactory, AbstractSshConnectionConfig connectionConfig) throws SshException, IOException {
+        connectionConfig.setHost("192.168.234.128"); //pc
+        // connectionConfig.setHost("192.168.1.70"); //work station
+        connectionConfig.setPort(22);
+        connectionConfig.setUser("fangjinuo");
+        connectionConfig.setPassword("fjn13570");
+        SshConnection connection = connectionFactory.get(connectionConfig);
+
+        SshCommandResponse response = null;
+        response = SshClients.exec(connection, "route");
+        showResult(response);
+        response = SshClients.exec(connection, "ifconfig");
+        showResult(response);
+
+        response = SshClients.exec(connection, "ls -al");
+        showResult(response);
+
+        // 测试错误命令
+        response = SshClients.exec(connection, "ls2 -al");
+        showResult(response);
+        connection.close();
+    }
+
+    private static void showResult(SshCommandResponse response) {
+        if (response.hasError()) {
+            logger.error(response.getExitErrorMessage());
+        } else {
+            logger.info(response.getResult());
+        }
+        System.out.println("========================");
+    }
+}
---
+++ agileway-ssh/agileway-sshclient-tests/src/test/java/com/jn/agileway/ssh/test/channel/direct/session/command/SshCommandTest.java
@@ -0,0 +1,106 @@
+package com.jn.agileway.ssh.test.channel.direct.session.command;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jn.agileway.ssh.client.SshConnection;
+import com.jn.agileway.ssh.client.SshConnectionConfig;
+import com.jn.agileway.ssh.client.SshConnectionFactory;
+import com.jn.agileway.ssh.client.SshConnectionFactoryRegistry;
+import com.jn.agileway.ssh.client.channel.SessionedChannel;
+import com.jn.agileway.ssh.client.impl.jsch.JschLoggerToSlf4jLogger;
+import com.jn.langx.util.io.IOs;
+import com.jn.langx.util.logging.Loggers;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SshCommandTest {
+    private static final Logger logger = Loggers.getLogger(SshCommandTest.class);
+
+    @Test
+    public void testUseJschAPI() throws Throwable {
+        String password = "fjn13570";
+        String username = "fangjinuo";
+        //  String host = "192.168.1.79";
+        String host = "192.168.234.128";
+        int port = 22;
+
+        JSch.setConfig("StrictHostKeyChecking", "false");
+
+        JSch jsch = new JSch();
+        // jsch.setKnownHosts("known_hosts");
+        JSch.setLogger(new JschLoggerToSlf4jLogger());
+        Session session = jsch.getSession(username, host, port);
+        session.setPassword(password);
+        session.connect();
+
+
+        executeAndDump(session, "route");
+        executeAndDump(session, "ifconfig");
+        executeAndDump(session, "cd ~/.java;ls -al");
+        session.disconnect();
+    }
+
+    private void executeAndDump(Session session, String command) throws JSchException, IOException {
+        logger.info("\n====== start execute: {} ======", command);
+        ChannelExec channel = (ChannelExec) session.openChannel("exec");
+        channel.setCommand(command);
+        channel.connect();
+        logger.info("isClosed: {}", channel.isClosed());
+        logger.info("isConnected: {}", channel.isConnected());
+        InputStream errorStream = channel.getErrStream();
+        InputStream outStream = channel.getInputStream();
+        String error = IOs.readAsString(errorStream);
+        logger.info(error);
+        String content = IOs.readAsString(outStream);
+        logger.info(content);
+        channel.disconnect();
+        logger.info("====== finish execute: {} ======", command);
+    }
+
+    @Test
+    public void testUseAgilewayJschAPI() throws Throwable {
+
+        SshConnectionFactoryRegistry registry = new SshConnectionFactoryRegistry();
+        SshConnectionFactory connectionFactory = registry.get("jsch");
+        SshConnectionConfig connectionConfig = connectionFactory.newConfig();
+
+        String password = "fjn13570";
+        String username = "fangjinuo";
+        //  String host = "192.168.1.79";
+        String host = "192.168.234.128";
+        int port = 22;
+
+        connectionConfig.setHost(host);
+        connectionConfig.setPort(port);
+        connectionConfig.setPassword(password);
+        connectionConfig.setUser(username);
+        SshConnection connection = connectionFactory.get(connectionConfig);
+
+
+        executeAndDump(connection, "route");
+        executeAndDump(connection, "ifconfig");
+        executeAndDump(connection, "cd ~/.java;ls -al");
+        connection.close();
+    }
+
+    private void executeAndDump(SshConnection connection, String command) throws JSchException, IOException {
+        logger.info("\n====== start execute: {} ======", command);
+        SessionedChannel channel = connection.openSession();
+
+        channel.exec(command);
+
+        //   System.out.println(channel.getExitStatus());
+        InputStream outStream = channel.getInputStream();
+        String content = IOs.readAsString(outStream);
+        logger.info(content);
+        channel.close();
+        logger.info("====== finish execute: {} ======", command);
+    }
+
+
+}
