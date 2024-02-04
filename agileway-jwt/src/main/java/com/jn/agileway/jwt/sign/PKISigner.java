f23017b0de287105424f4e8505e510a85c5ab5c5
fjn1194361820 <fs1194361820@163.com>
2024-02-04 17:16:26 +0800

    * 支持 pki 进行签名

--- agileway-jwt/src/main/java/com/jn/agileway/jwt/sign/PKISigner.java
+++ agileway-jwt/src/main/java/com/jn/agileway/jwt/sign/PKISigner.java
@@ -6,6 +6,7 @@ import com.jn.agileway.jwt.Signer;
 import com.jn.langx.codec.base64.Base64;
 import com.jn.langx.security.crypto.signature.Signatures;
 import com.jn.langx.text.StringTemplates;
+import com.jn.langx.util.Objs;
 import com.jn.langx.util.collection.Lists;
 import com.jn.langx.util.collection.Maps;
 import com.jn.langx.util.function.Supplier;
@@ -13,6 +14,7 @@ import com.jn.langx.util.io.Charsets;
 
 import java.security.PrivateKey;
 import java.security.Signature;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.AlgorithmParameterSpec;
 import java.security.spec.MGF1ParameterSpec;
 import java.security.spec.PSSParameterSpec;
@@ -70,13 +72,51 @@ public class PKISigner implements Signer {
                 return signer;
             }
         });
+        map.put("ES256",new Supplier<PrivateKey, Signature>(){
+            @Override
+            public Signature get(PrivateKey privateKey) {
+                AlgorithmParameterSpec parameterSpec = new PSSParameterSpec("SHA-512", "MGF1", new MGF1ParameterSpec("SHA-512"), 64, 1);
+                Signature signer= Signatures.createSignature("SHA256withECDSA",null, privateKey,null, parameterSpec);
+                return signer;
+            }
+        });
+        map.put("ES256K",new Supplier<PrivateKey, Signature>(){
+            @Override
+            public Signature get(PrivateKey privateKey) {
+                AlgorithmParameterSpec parameterSpec =new PSSParameterSpec("SHA-512", "MGF1", new MGF1ParameterSpec("SHA-512"), 64, 1);
+                Signature signer= Signatures.createSignature("SHA256withECDSA",null, privateKey,null, parameterSpec);
+                return signer;
+            }
+        });
+        map.put("ES384",new Supplier<PrivateKey, Signature>(){
+            @Override
+            public Signature get(PrivateKey privateKey) {
+                AlgorithmParameterSpec parameterSpec =new PSSParameterSpec("SHA-512", "MGF1", new MGF1ParameterSpec("SHA-512"), 64, 1);
+                Signature signer= Signatures.createSignature("SHA384withECDSA",null, privateKey,null, parameterSpec);
+                return signer;
+            }
+        });
+        map.put("ES512",new Supplier<PrivateKey, Signature>(){
+            @Override
+            public Signature get(PrivateKey privateKey) {
+                AlgorithmParameterSpec parameterSpec =new PSSParameterSpec("SHA-512", "MGF1", new MGF1ParameterSpec("SHA-512"), 64, 1);
+                Signature signer= Signatures.createSignature("SHA512withECDSA",null, privateKey,null, parameterSpec);
+                return signer;
+            }
+        });
         jwtAlgorithmToPKI=map;
     }
 
     private final PrivateKey privateKey;
 
     public PKISigner(PrivateKey privateKey){
-        this.privateKey = privateKey;
+        String algorithm= privateKey.getAlgorithm();
+        if (privateKey instanceof RSAPrivateKey || Objs.equals("RSA",algorithm) ||Objs.equals("EC",algorithm)  ) {
+            // Will also allow "RSASSA-PSS" alg RSAPrivateKey instances with MGF1ParameterSpec
+            this.privateKey = privateKey;
+        } else {
+            throw new IllegalArgumentException("The private key algorithm must be RSA");
+        }
     }
 
     @Override
