f885f416b2cb2e52769ef82d6fded9d7a309b9ea
fjn1194361820 <fs1194361820@163.com>
2024-08-05 20:15:29 +0800

    * 移除 agileway-eipchannel-kafka模块

--- agileway-eipchannel/agileway-eipchannel-kafka/src/main/java/com/jn/agileway/eipchannel/kafka/KafkaPollingEventConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.jn.agileway.eipchannel.kafka;
-
-
-import com.jn.agileway.eipchannel.core.endpoint.pubsub.PollingConsumer;
-
-public class KafkaPollingEventConsumer extends PollingConsumer {
-
-}
