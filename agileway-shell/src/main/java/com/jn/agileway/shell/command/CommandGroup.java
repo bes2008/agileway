c1eaf1e4525ca37f6950b48a899b98fa5aaddafb
fjn1194361820 <fs1194361820@163.com>
2025-01-17 14:29:28 +0800

    + 改造命令模式

--- agileway-shell/src/main/java/com/jn/agileway/shell/command/CommandGroup.java
+++ agileway-shell/src/main/java/com/jn/agileway/shell/command/CommandGroup.java
@@ -1,20 +1,10 @@
 package com.jn.agileway.shell.command;
 
-import com.jn.langx.util.collection.Collects;
-
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 public class CommandGroup {
     public static final String DEFAULT_GROUP="DEFAULT";
     private String name;
     private String desc;
-    private final Map<String, Command> definitionMap = new LinkedHashMap<>();
-
-    public void addCommand(Command command){
-        this.definitionMap.put(command.getName(),command);
-    }
 
     public String getName() {
         return name;
@@ -22,14 +12,6 @@ public class CommandGroup {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<Command> getCommands(){
-        return Collects.asList(definitionMap.values());
-    }
-
-    public Command getCommand(String commandName){
-        return definitionMap.get(commandName);
     }
 
     public String getDesc() {
--- agileway-shell/src/main/java/com/jn/agileway/shell/command/CommandRegistry.java
+++ agileway-shell/src/main/java/com/jn/agileway/shell/command/CommandRegistry.java
@@ -1,67 +1,32 @@
 package com.jn.agileway.shell.command;
 
-import com.jn.langx.util.collection.Maps;
-import com.jn.langx.util.collection.Pipeline;
-import com.jn.langx.util.function.Consumer;
-import com.jn.langx.util.function.Supplier0;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class CommandRegistry {
-    private Map<String,CommandGroup> commandGroupMap=new HashMap<String,CommandGroup>();
+    private Map<String, CommandGroup> commandGroupMap = new HashMap<>();
+    private Map<String,Command> commandMap=new HashMap<String,Command>();
 
     public Command getCommand(String command){
-        return getCommand(CommandGroup.DEFAULT_GROUP, command);
+        return commandMap.get(command);
     }
 
     public CommandGroup getCommandGroup(String group){
-        return commandGroupMap.get(group);
-    }
-
-    private CommandGroup getOrCreateCommandGroup(String group){
-        return Maps.get(commandGroupMap, group, new Supplier0<CommandGroup>() {
-            @Override
-            public CommandGroup get() {
-                return new CommandGroup();
-            }
-        });
-    }
-
-
-    public Command getCommand(String group, String command){
-        CommandGroup commandGroup = getCommandGroup(group);
-        if(commandGroup==null){
-            return null;
-        }
-        return commandGroup.getCommand(command);
-    }
-
-    public void addCommand(String group, Command command){
-        command.setGroup(group);
-        getOrCreateCommandGroup(group).addCommand(command);
+        return this.commandGroupMap.get(group);
     }
 
     public void addCommand(Command command){
         if(command==null){
             return;
         }
-        String group = command.getGroup();
-        if(group==null){
-            group=CommandGroup.DEFAULT_GROUP;
-        }
-        addCommand(group, command);
+        this.commandMap.put(command.getName(), command);
     }
 
     public void addCommandGroup(CommandGroup commandGroup){
         if(commandGroup==null){
             return;
         }
-        Pipeline.of(commandGroup.getCommands()).forEach(new Consumer<Command>() {
-            @Override
-            public void accept(Command command) {
-                addCommand(command);
-            }
-        });
+        this.commandGroupMap.put(commandGroup.getName(), commandGroup);
     }
 }
