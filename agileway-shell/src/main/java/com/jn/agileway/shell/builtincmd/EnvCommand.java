c54f3bc37fe246437203eb09ff9c3162d85caf73
fjn1194361820 <fs1194361820@163.com>
2025-01-17 18:32:39 +0800

    + 提供系统属性命令

--- agileway-shell/src/main/java/com/jn/agileway/shell/builtincmd/EnvCommand.java
+++ agileway-shell/src/main/java/com/jn/agileway/shell/builtincmd/EnvCommand.java
@@ -3,10 +3,14 @@ package com.jn.agileway.shell.builtincmd;
 import com.jn.agileway.shell.command.annotation.Command;
 import com.jn.agileway.shell.command.annotation.CommandComponent;
 import com.jn.agileway.shell.command.annotation.CommandOption;
+import com.jn.langx.text.properties.Props;
 import com.jn.langx.util.Strings;
+import com.jn.langx.util.SystemPropertys;
+import com.jn.langx.util.collection.Collects;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @CommandComponent
 
@@ -25,6 +29,28 @@ public class EnvCommand {
             String envName = entry.getKey();
             String envValue = entry.getValue();
             if (Strings.contains(envName, search, true) || Strings.contains(envValue, search, true)){
+                result.put(envName, envValue);
+            }
+        }
+        return result;
+    }
+
+    @Command("system-props")
+    public Map<String, String> systemProperties(
+            @CommandOption(value = "s", longName = "search", required = false)
+            String search ){
+        search = Strings.trimToNull(search);
+        Map<String, String> properties = Props.toStringMap(System.getProperties());
+
+        if(search==null){
+            return properties;
+        }
+
+        Map<String, String> result = new HashMap<>();
+        for (Map.Entry<String, String> entry : properties.entrySet()) {
+            String envName = entry.getKey();
+            String envValue = entry.getValue();
+            if (Strings.contains(envName, search, true) || Strings.contains(envValue, search, true)) {
                 result.put(envName, envValue);
             }
         }
