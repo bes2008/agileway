7be9c3c9eb8244cfb53b168eb922274fda81987b
coderjn <fs1194361820@163.com>
2025-01-21 17:02:16 +0800

    * 调整输出格式

--- agileway-shell/src/main/java/com/jn/agileway/shell/builtin/UsageCommands.java
+++ agileway-shell/src/main/java/com/jn/agileway/shell/builtin/UsageCommands.java
@@ -9,27 +9,35 @@ import com.jn.agileway.shell.command.annotation.CommandOption;
 import com.jn.langx.util.Strings;
 
 import java.util.List;
+
 @CommandComponent
 public class UsageCommands {
 
     private CommandRegistry commandRegistry;
 
-    public UsageCommands(CommandRegistry commandRegistry){
+    public UsageCommands(CommandRegistry commandRegistry) {
         this.commandRegistry = commandRegistry;
     }
 
     @Command(value = "commands", desc = "list commands in some or all groups")
     public String listCommands(
-            @CommandOption(value = "g",longName = "groups", required = false)
-           String[] groupNames) {
+            @CommandOption(value = "g", longName = "groups", required = false)
+            String[] groupNames) {
         StringBuilder builder = new StringBuilder(255);
         List<CommandGroup> commandGroups = commandRegistry.getCommandGroups(groupNames);
-        for (CommandGroup commandGroup: commandGroups){
-            String groupName= commandGroup.getName();
-            builder.append(new AnsiFontText(Strings.completingLength(groupName,16, Strings.SP, false)).bold(true)).append("\t-\t").append(commandGroup.getDesc()).append(Strings.CRLF);
-            List< com.jn.agileway.shell.command.Command> cmds = this.commandRegistry.getGroupCommands(groupName);
-            for (com.jn.agileway.shell.command.Command cmd:cmds){
-                builder.append("\t").append(new AnsiFontText(Strings.completingLength(cmd.getName(),12,Strings.SP,false)).bold(true)).append("\t-\t").append( cmd.getDesc()).append(Strings.CRLF);
+        for (CommandGroup commandGroup : commandGroups) {
+            String groupName = commandGroup.getName();
+            builder.append(new AnsiFontText(Strings.completingLength(groupName, 16, Strings.SP, false)).bold(true))
+                    .append("\t-\t")
+                    .append(commandGroup.getDesc())
+                    .append(Strings.CRLF);
+            List<com.jn.agileway.shell.command.Command> cmds = this.commandRegistry.getGroupCommands(groupName);
+            for (com.jn.agileway.shell.command.Command cmd : cmds) {
+                builder.append("\t")
+                        .append(new AnsiFontText(Strings.completingLength(cmd.getName(), 12, Strings.SP, false)).bold(true))
+                        .append("\t-\t")
+                        .append(cmd.getDesc())
+                        .append(Strings.CRLF);
             }
             builder.append(Strings.CRLF).append(Strings.CRLF);
         }
