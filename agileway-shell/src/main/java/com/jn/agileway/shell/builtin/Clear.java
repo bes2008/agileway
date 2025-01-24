23157c5328ba39e0c8182b78b853a3bc0a6f561b
fjn1194361820 <fs1194361820@163.com>
2025-01-24 11:59:05 +0800

    * 支持 clean

--- agileway-shell/src/main/java/com/jn/agileway/shell/builtin/Clear.java
+++ agileway-shell/src/main/java/com/jn/agileway/shell/builtin/Clear.java
@@ -2,12 +2,15 @@ package com.jn.agileway.shell.builtin;
 
 import com.jn.agileway.shell.command.annotation.Command;
 import com.jn.agileway.shell.command.annotation.CommandComponent;
+import org.fusesource.jansi.AnsiConsole;
 
 @CommandComponent
 public class Clear {
     @Command(value = "clear", alias = {"cls","clean"}, desc = "clear screen")
     public void clear(){
-        System.out.print("\033[2J\033[H");
-        System.out.flush();
+        if (AnsiConsole.isInstalled()) {
+            System.out.print("\033[2J\033[H");
+            System.out.flush();
+        }
     }
 }
