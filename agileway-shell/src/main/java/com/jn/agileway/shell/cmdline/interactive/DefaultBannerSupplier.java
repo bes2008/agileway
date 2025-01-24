136c3b416ad01a04094bd59147aa65d6c8c2f87f
fjn1194361820 <fs1194361820@163.com>
2025-01-24 11:39:18 +0800

    * 更新banner

--- agileway-shell/src/main/java/com/jn/agileway/shell/cmdline/interactive/DefaultBannerSupplier.java
+++ agileway-shell/src/main/java/com/jn/agileway/shell/cmdline/interactive/DefaultBannerSupplier.java
@@ -1,13 +1,14 @@
 package com.jn.agileway.shell.cmdline.interactive;
 
 public class DefaultBannerSupplier implements BannerSupplier{
-    String str = "             _ _                          \n" +
-            "  __ _  __ _(_) | _____      ____ _ _   _ \n" +
-            " / _` |/ _` | | |/ _ \\ \\ /\\ / / _` | | | |\n" +
-            "| (_| | (_| | | |  __/\\ V  V / (_| | |_| |\n" +
-            " \\__,_|\\__, |_|_|\\___| \\_/\\_/ \\__,_|\\__, |\n" +
-            "       |___/                        |___/ \n" +
-            "Welcome to use agileway-shell, have fun. ";
+    String str = "               _  _                              \n" +
+            "  __ _   __ _ (_)| |  ___ __      __ __ _  _   _ \n" +
+            " / _` | / _` || || | / _ \\\\ \\ /\\ / // _` || | | |\n" +
+            "| (_| || (_| || || ||  __/ \\ V  V /| (_| || |_| |\n" +
+            " \\__,_| \\__, ||_||_| \\___|  \\_/\\_/  \\__,_| \\__, |\n" +
+            "        |___/                              |___/ \n" +
+            "                                                 \n"+
+            " Welcome to use agileway shell. Have fun.\n\n";
     @Override
     public String get() {
         return str;
