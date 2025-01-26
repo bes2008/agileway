6d47dce9ce6937ba7f9fa4de13b4f19d766678f2
coderjn <fs1194361820@163.com>
2025-01-26 18:53:54 +0800

    + 提供脚本执行模式

--- agileway-shell/src/main/java/com/jn/agileway/shell/builtin/Script.java
+++ agileway-shell/src/main/java/com/jn/agileway/shell/builtin/Script.java
@@ -1,4 +1,26 @@
 package com.jn.agileway.shell.builtin;
 
+import com.jn.agileway.shell.Shell;
+import com.jn.agileway.shell.cmdline.script.FileCmdlineProvider;
+import com.jn.agileway.shell.command.annotation.Command;
+import com.jn.agileway.shell.command.annotation.CommandArgument;
+import com.jn.agileway.shell.command.annotation.CommandComponent;
+
+@CommandComponent
 public class Script {
+    private Shell shell;
+
+    public Script(){
+    }
+
+    public void setShell(Shell shell) {
+        this.shell = shell;
+    }
+
+    @Command
+    public void execScript(
+            @CommandArgument(value = "file", desc = "the file path")
+            String filepath){
+        this.shell.run(new FileCmdlineProvider(null, filepath));
+    }
 }
