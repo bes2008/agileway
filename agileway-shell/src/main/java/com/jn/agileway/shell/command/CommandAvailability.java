96d3b60f119c6c23c620ca808958ed30b750b995
fjn1194361820 <fs1194361820@163.com>
2025-02-17 09:29:48 +0800

    * 优化 CommandAvailability 的处理

--- agileway-shell/src/main/java/com/jn/agileway/shell/command/CommandAvailability.java
+++ agileway-shell/src/main/java/com/jn/agileway/shell/command/CommandAvailability.java
@@ -1,19 +1,36 @@
 package com.jn.agileway.shell.command;
 
 import com.jn.langx.AbstractNameable;
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.annotation.Nullable;
 import com.jn.langx.util.Objs;
+import com.jn.langx.util.collection.Lists;
+import com.jn.langx.util.pattern.glob.GlobMatcher;
 import com.jn.langx.util.reflect.Reflects;
 
 import java.lang.reflect.Method;
+import java.util.List;
 
 public class CommandAvailability extends AbstractNameable {
+    @NonNull
     private Method method;
+    @Nullable
     private String[] commands;
-
+    @NonNull
+    private List<GlobMatcher> globMatchers;
     public CommandAvailability(Method method, String[] commands) {
-        this.commands = commands;
         this.method = method;
         this.name = method.getName();
+        this.commands = commands;
+        List<GlobMatcher> matchers = Lists.newArrayList();
+        if(Objs.isNotEmpty(commands)){
+            for (String command : commands) {
+                GlobMatcher globMatcher = new GlobMatcher();
+                globMatcher.setPatternExpression(command);
+                matchers.add(globMatcher);
+            }
+        }
+        this.globMatchers = matchers;
     }
 
     public Method getMethod() {
@@ -25,14 +42,15 @@ public class CommandAvailability extends AbstractNameable {
     }
 
     public boolean isAvailableFor(Command command) {
-        if (commands == null || commands.length == 0) {
+        if(!Reflects.isSubClassOrEquals(this.getMethod().getDeclaringClass(), command.getMethod().getDeclaringClass())) {
+            return false;
+        }
+        if (globMatchers.isEmpty()) {
             return true;
         }
-        for (String cmd : commands) {
-            if (Objs.equals(cmd, command.getName())) {
-                if(Reflects.isSubClassOrEquals(this.getMethod().getDeclaringClass(), command.getMethod().getDeclaringClass())) {
-                    return true;
-                }
+        for (GlobMatcher matcher : this.globMatchers) {
+            if (Boolean.TRUE.equals(matcher.matches(command.getName()))) {
+                return true;
             }
         }
         return false;
