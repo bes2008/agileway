a7c720aaa49ab6132a55787ec1fe19b34ddb576f
fjn1194361820 <fs1194361820@163.com>
2025-05-07 14:04:49 +0800

    * 完善 jdk http 响应

--- agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/jdk/JdkHttpResponse.java
+++ agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/jdk/JdkHttpResponse.java
@@ -1,6 +1,7 @@
 package com.jn.agileway.httpclient.jdk;
 
 import com.jn.agileway.httpclient.core.HttpResponse;
+import com.jn.langx.util.Strings;
 import com.jn.langx.util.net.http.HttpHeaders;
 
 import java.io.IOException;
@@ -10,6 +11,9 @@ import java.net.HttpURLConnection;
 public class JdkHttpResponse implements HttpResponse {
 
     private HttpURLConnection httpConnection;
+    private HttpHeaders headers;
+
+    private InputStream responseStream;
 
     JdkHttpResponse(HttpURLConnection httpConnection) {
         this.httpConnection = httpConnection;
@@ -17,21 +21,46 @@ public class JdkHttpResponse implements HttpResponse {
 
     @Override
     public int getStatusCode() {
-        return 0;
+        try {
+            return this.httpConnection.getResponseCode();
+        } catch (IOException ex) {
+            return -1;
+        }
     }
 
     @Override
     public void close() throws IOException {
-
+        this.httpConnection.disconnect();
     }
 
     @Override
     public InputStream getBody() throws IOException {
-        return null;
+        if (this.responseStream == null) {
+            InputStream errorStream = this.httpConnection.getErrorStream();
+            this.responseStream = (errorStream != null ? errorStream : this.httpConnection.getInputStream());
+        }
+        return this.responseStream;
     }
 
     @Override
     public HttpHeaders getHeaders() {
-        return null;
+        if (this.headers == null) {
+            this.headers = new HttpHeaders();
+            // Header field 0 is the status line for most HttpURLConnections, but not on GAE
+            String name = this.httpConnection.getHeaderFieldKey(0);
+            if (Strings.isNotBlank(name)) {
+                this.headers.add(name, this.httpConnection.getHeaderField(0));
+            }
+            int i = 1;
+            while (true) {
+                name = this.httpConnection.getHeaderFieldKey(i);
+                if (Strings.isBlank(name)) {
+                    break;
+                }
+                this.headers.add(name, this.httpConnection.getHeaderField(i));
+                i++;
+            }
+        }
+        return this.headers;
     }
 }
