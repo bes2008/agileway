a1d38922c9bb9997a305a87eeb14f2cd31d13fe1
coderjn <fs1194361820@163.com>
2025-05-15 07:12:51 +0800

    * 准备builder

---
+++ agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/core/UnderlyingHttpRequestFactoryBuilder.java
@@ -0,0 +1,28 @@
+package com.jn.agileway.httpclient.core;
+
+import com.jn.langx.Builder;
+import com.jn.langx.security.ssl.SSLContextBuilder;
+
+import javax.net.ssl.HostnameVerifier;
+import java.lang.reflect.Proxy;
+
+public interface UnderlyingHttpRequestFactoryBuilder extends Builder<UnderlyingHttpRequestFactory> {
+    UnderlyingHttpRequestFactoryBuilder poolMaxIdleConnections(int maxIdleConnections);
+
+    UnderlyingHttpRequestFactoryBuilder keepAliveDurationMills(int keepAliveDurationInMills);
+
+    UnderlyingHttpRequestFactoryBuilder connectTimeoutMills(int connectTimeoutInMills);
+
+    UnderlyingHttpRequestFactoryBuilder readTimeoutMills(int readTimeoutInMills);
+
+    UnderlyingHttpRequestFactoryBuilder proxy(Proxy proxy);
+
+    UnderlyingHttpRequestFactoryBuilder hostnameVerifier(HostnameVerifier hostnameVerifier);
+
+    UnderlyingHttpRequestFactoryBuilder retry(int retry);
+
+    UnderlyingHttpRequestFactoryBuilder sslContextBuilder(SSLContextBuilder sslContextBuilder);
+
+    @Override
+    UnderlyingHttpRequestFactory build();
+}
