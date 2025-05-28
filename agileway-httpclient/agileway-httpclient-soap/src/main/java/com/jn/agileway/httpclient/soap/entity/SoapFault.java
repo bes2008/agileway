d6d1e561a5d706f2cefef754b8780761c2b4d8cf
fjn1194361820 <fs1194361820@163.com>
2025-05-28 11:18:03 +0800

    * 规范 soap fault

--- agileway-httpclient/agileway-httpclient-soap/src/main/java/com/jn/agileway/httpclient/soap/entity/SoapFault.java
+++ agileway-httpclient/agileway-httpclient-soap/src/main/java/com/jn/agileway/httpclient/soap/entity/SoapFault.java
@@ -3,14 +3,89 @@ package com.jn.agileway.httpclient.soap.entity;
 import java.net.URI;
 import java.util.Map;
 
-public interface SoapFault {
-    Map<String, String> getDetail();
+public class SoapFault {
+    /**
+     * SOAP Fault code, V1.1, V1.2都有，名字不一样
+     */
+    private String code;
+    /**
+     * SOAP Fault sub code, V1.2才有
+     */
+    String subCode;
+    /**
+     * SOAP Fault reason, V1.1, V1.2都有
+     */
+    private String reason;
+    /**
+     * SOAP Fault role, actor, V1.1, V1.2都有，名字不一样
+     */
+    private URI role;
+    /**
+     * SOAP Fault node, V1.2才有
+     */
+    private URI node;
+    /**
+     * SOAP Fault detail, V1.1, V1.2都有
+     */
+    private Map<String, String> detail;
 
-    String getCode();
+    public SoapFault() {
+    }
 
-    String getSubCode();
+    public SoapFault(String code, String subCode, String reason, URI role, URI node, Map<String, String> detail) {
+        this.code = code;
+        this.subCode = subCode;
+        this.reason = reason;
+        this.role = role;
+        this.node = node;
+        this.detail = detail;
+    }
 
-    String getReason();
+    public Map<String, String> getDetail() {
+        return this.detail;
+    }
 
-    URI getRole();
+    public String getCode() {
+        return this.code;
+    }
+
+    public String getSubCode() {
+        return this.subCode;
+    }
+
+    public String getReason() {
+        return this.reason;
+    }
+
+    public URI getRole() {
+        return this.role;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setSubCode(String subCode) {
+        this.subCode = subCode;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setRole(URI role) {
+        this.role = role;
+    }
+
+    public URI getNode() {
+        return node;
+    }
+
+    public void setNode(URI node) {
+        this.node = node;
+    }
+
+    public void setDetail(Map<String, String> detail) {
+        this.detail = detail;
+    }
 }
