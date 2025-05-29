cefd72245fed19709082d0f14f00b1a194e7a32f
fjn1194361820 <fs1194361820@163.com>
2025-05-29 18:29:09 +0800

    * 设置content-type for SOAP

--- agileway-httpclient/agileway-httpclient-soap/src/main/java/com/jn/agileway/httpclient/soap/SoapExchanger.java
+++ agileway-httpclient/agileway-httpclient-soap/src/main/java/com/jn/agileway/httpclient/soap/SoapExchanger.java
@@ -3,7 +3,7 @@ package com.jn.agileway.httpclient.soap;
 import com.jn.agileway.httpclient.core.HttpExchanger;
 import com.jn.agileway.httpclient.core.HttpRequest;
 import com.jn.agileway.httpclient.core.HttpResponse;
-import com.jn.agileway.httpclient.soap.entity.SoapMessage;
+import com.jn.agileway.httpclient.soap.entity.SoapBinding;
 import com.jn.langx.util.concurrent.promise.Promise;
 
 public class SoapExchanger {
@@ -13,21 +13,13 @@ public class SoapExchanger {
         this.httpExchanger = exchanger;
     }
 
-    public <T> HttpResponse<T> exchange(String uri, SoapMessage soapMessage, Class<T> expectedContentType) {
-        return this.exchangeAsync(uri, soapMessage, expectedContentType).await();
+    public <T> HttpResponse<T> exchange(String uri, SoapBinding binding, Object soapMessage, Class<T> expectedContentType) {
+        return this.exchangeAsync(uri, binding, soapMessage, expectedContentType).await();
     }
 
-    public <T> Promise<HttpResponse<T>> exchangeAsync(String uri, SoapMessage soapMessage, Class<T> expectedContentType) {
+    public <T> Promise<HttpResponse<T>> exchangeAsync(String uri, SoapBinding binding, Object soapMessage, Class<T> expectedContentType) {
         HttpRequest request = HttpRequest.forPost(uri, null, null, soapMessage);
-        return httpExchanger.exchange(true, request, expectedContentType);
-    }
-
-    public <T> HttpResponse<T> exchange(String uri, Object soapMessage, Class<T> expectedContentType) {
-        return this.exchangeAsync(uri, soapMessage, expectedContentType).await();
-    }
-
-    public <T> Promise<HttpResponse<T>> exchangeAsync(String uri, Object soapMessage, Class<T> expectedContentType) {
-        HttpRequest request = HttpRequest.forPost(uri, null, null, soapMessage);
+        request.getHeaders().setContentType(binding.getContentType());
         return httpExchanger.exchange(true, request, expectedContentType);
     }
 
