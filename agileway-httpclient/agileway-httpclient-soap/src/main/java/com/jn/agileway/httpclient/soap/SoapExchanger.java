e5b6b7cf40fff2b9b2c00fde0b7881ba6bed5325
fjn1194361820 <fs1194361820@163.com>
2025-05-29 18:10:58 +0800

    * 简化代码

--- agileway-httpclient/agileway-httpclient-soap/src/main/java/com/jn/agileway/httpclient/soap/SoapExchanger.java
+++ agileway-httpclient/agileway-httpclient-soap/src/main/java/com/jn/agileway/httpclient/soap/SoapExchanger.java
@@ -4,10 +4,7 @@ import com.jn.agileway.httpclient.core.HttpExchanger;
 import com.jn.agileway.httpclient.core.HttpRequest;
 import com.jn.agileway.httpclient.core.HttpResponse;
 import com.jn.agileway.httpclient.soap.entity.SoapMessage;
-import com.jn.langx.util.collection.multivalue.MultiValueMap;
 import com.jn.langx.util.concurrent.promise.Promise;
-
-import java.util.Map;
 
 public class SoapExchanger {
     private HttpExchanger httpExchanger;
@@ -16,8 +13,8 @@ public class SoapExchanger {
         this.httpExchanger = exchanger;
     }
 
-    public <T> Promise<HttpResponse<T>> exchange(boolean async, String uri, MultiValueMap<String, Object> queryParams, Map<String, Object> uriVariables, SoapMessage soapMessage, Class<T> expectedContentType) {
-        HttpRequest request = HttpRequest.forPost(uri, queryParams, uriVariables, soapMessage);
+    public <T> Promise<HttpResponse<T>> exchange(boolean async, String uri, SoapMessage soapMessage, Class<T> expectedContentType) {
+        HttpRequest request = HttpRequest.forPost(uri, null, null, soapMessage);
         return httpExchanger.exchange(async, request, expectedContentType);
     }
 
