304bb907839ab4f2a8524ddbcedd588bcf709d20
fjn1194361820 <fs1194361820@163.com>
2025-05-30 16:01:49 +0800

    * message 对接

--- agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/core/HttpMessage.java
+++ agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/core/HttpMessage.java
@@ -1,16 +1,15 @@
 package com.jn.agileway.httpclient.core;
 
+import com.jn.agileway.eipchannel.core.message.Message;
 import com.jn.langx.util.net.http.HttpHeaders;
 import com.jn.langx.util.net.http.HttpMethod;
 
 import java.net.URI;
 
-public interface HttpMessage<T> {
+public interface HttpMessage<T> extends Message<T> {
     URI getUri();
 
     HttpMethod getMethod();
 
     HttpHeaders getHttpHeaders();
-
-    T getPayload();
 }
