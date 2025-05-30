91dd65bbee361f30f3c808051a7ed6a0d55c5cbf
coderjn <fs1194361820@163.com>
2025-05-30 08:36:54 +0800

    * update it

--- agileway-httpclient/agileway-httpclient-soap/src/main/java/com/jn/agileway/httpclient/soap/SoapExchanger.java
+++ agileway-httpclient/agileway-httpclient-soap/src/main/java/com/jn/agileway/httpclient/soap/SoapExchanger.java
@@ -5,11 +5,15 @@ import com.jn.agileway.httpclient.core.HttpRequest;
 import com.jn.agileway.httpclient.core.HttpResponse;
 import com.jn.agileway.httpclient.soap.entity.SoapBinding;
 import com.jn.agileway.httpclient.soap.plugin.SoapFaultResponseExtractor;
+import com.jn.langx.util.collection.multivalue.MultiValueMap;
 import com.jn.langx.util.concurrent.promise.Promise;
+
+import java.util.Map;
 
 public class SoapExchanger {
     private HttpExchanger httpExchanger;
     private SoapFaultResponseExtractor soapFaultResponseExtractor = new SoapFaultResponseExtractor();
+
     public SoapExchanger(HttpExchanger exchanger) {
         this.httpExchanger = exchanger;
     }
@@ -18,8 +22,16 @@ public class SoapExchanger {
         return this.exchangeAsync(uri, binding, soapMessage, expectedContentType).await();
     }
 
+    public <T> HttpResponse<T> exchange(String uri, MultiValueMap<String, Object> queryParams, Map<String, Object> uriVariables, SoapBinding binding, Object soapMessage, Class<T> expectedContentType) {
+        return this.exchangeAsync(uri, queryParams, uriVariables, binding, soapMessage, expectedContentType).await();
+    }
+
     public <T> Promise<HttpResponse<T>> exchangeAsync(String uri, SoapBinding binding, Object soapMessage, Class<T> expectedContentType) {
-        HttpRequest request = HttpRequest.forPost(uri, null, null, soapMessage);
+        return this.exchangeAsync(uri, null, null, binding, soapMessage, expectedContentType);
+    }
+
+    public <T> Promise<HttpResponse<T>> exchangeAsync(String uri, MultiValueMap<String, Object> queryParams, Map<String, Object> uriVariables, SoapBinding binding, Object soapMessage, Class<T> expectedContentType) {
+        HttpRequest request = HttpRequest.forPost(uri, queryParams, uriVariables, soapMessage);
         request.getHeaders().setContentType(binding.getContentType());
         return httpExchanger.exchange(true, request, expectedContentType, null, soapFaultResponseExtractor);
     }
