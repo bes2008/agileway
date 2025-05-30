ea51e5924351f5e9dc72a2961e81956a8e581a2b
fjn1194361820 <fs1194361820@163.com>
2025-05-30 17:21:54 +0800

    * update it

--- agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/core/HttpRequestReplyExchanger.java
+++ agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/core/HttpRequestReplyExchanger.java
@@ -1,11 +1,44 @@
 package com.jn.agileway.httpclient.core;
 
+import com.jn.agileway.eipchannel.core.channel.pipe.PipedDuplexChannel;
 import com.jn.agileway.eipchannel.core.endpoint.exchange.RequestReplyExchanger;
 import com.jn.agileway.eipchannel.core.message.Message;
 import com.jn.langx.lifecycle.AbstractLifecycle;
 import com.jn.langx.util.concurrent.promise.Promise;
 
 public class HttpRequestReplyExchanger extends AbstractLifecycle implements RequestReplyExchanger {
+
+    /**
+     * <pre>
+     *  OutboundChannel                             InboundChannel
+     *
+     *
+     *  HttpRequest&lt;JavaBean>                   HttpResponse&lt;JavaBean>
+     *        |                                             ^
+     *        v                                             |
+     *  ---------------------                       ---------------------
+     *  |   Interceptors    |                       |   Interceptors    |
+     *  ---------------------                       ---------------------
+     *        |                                             ^
+     *        v                                             |
+     *  HttpRequest&lt;ByteArrayOutputStream>      HttpResponse&lt;ByteArrayInputStream>
+     *        |
+     *        v
+     *  ---------------------                       ---------------------
+     *  |   Interceptors    |                       |   Interceptors    |
+     *  ---------------------                       ---------------------
+     *        |                                             ^
+     *        v                                             |
+     *  UnderlyingHttpRequest                      UnderlyingHttpResponse
+     *        |                                             ^
+     *        v                                             |
+     * ---------------------------------------------------------------------------
+     *         |------------> Transport Level ------------>|
+     * ---------------------------------------------------------------------------
+     * </pre>
+     */
+    private PipedDuplexChannel requestReplyChannel;
+
     @Override
     public Promise<Message<?>> exchangeAsync(Message<?> request) {
         return null;
