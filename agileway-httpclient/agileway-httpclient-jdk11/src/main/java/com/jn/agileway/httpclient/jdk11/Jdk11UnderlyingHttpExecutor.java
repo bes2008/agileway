1c0f1304ddf4a3f218e80f328e112d02cf922a1c
fjn1194361820 <fs1194361820@163.com>
2025-06-03 12:01:10 +0800

    * 提供文件上传的支持

--- agileway-httpclient/agileway-httpclient-jdk11/src/main/java/com/jn/agileway/httpclient/jdk11/Jdk11UnderlyingHttpExecutor.java
+++ agileway-httpclient/agileway-httpclient-jdk11/src/main/java/com/jn/agileway/httpclient/jdk11/Jdk11UnderlyingHttpExecutor.java
@@ -1,7 +1,6 @@
 package com.jn.agileway.httpclient.jdk11;
 
 import com.jn.agileway.httpclient.core.payload.HttpRequestPayloadWriter;
-import com.jn.agileway.httpclient.core.payload.multipart.MultiPartsForm;
 import com.jn.agileway.httpclient.core.underlying.AbstractUnderlyingHttpExecutor;
 import com.jn.agileway.httpclient.core.underlying.UnderlyingHttpResponse;
 import com.jn.agileway.httpclient.util.ContentEncoding;
@@ -9,12 +8,12 @@ import com.jn.agileway.httpclient.util.HttpClientUtils;
 import com.jn.langx.util.Objs;
 import com.jn.langx.util.Strings;
 import com.jn.langx.util.Throwables;
+import com.jn.langx.util.io.IOs;
+import com.jn.langx.util.logging.Loggers;
 import com.jn.langx.util.net.http.HttpHeaders;
 import com.jn.langx.util.net.http.HttpMethod;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -48,8 +47,53 @@ public class Jdk11UnderlyingHttpExecutor extends AbstractUnderlyingHttpExecutor<
     }
 
     @Override
-    public UnderlyingHttpResponse executeAttachmentUploadRequest(com.jn.agileway.httpclient.core.HttpRequest<?> request, HttpRequestPayloadWriter payloadWriter) throws Exception {
-        return null;
+    public UnderlyingHttpResponse executeAttachmentUploadRequest(com.jn.agileway.httpclient.core.HttpRequest<?> request, final HttpRequestPayloadWriter payloadWriter) throws Exception {
+        HttpMethod method = request.getMethod();
+        URI uri = request.getUri();
+        HttpRequest.Builder builder = HttpRequest.newBuilder(uri);
+        writeHeaders(request, builder);
+        if (HttpClientUtils.isWriteableMethod(request.getMethod()) && request.getPayload() != null) {
+
+            PipedInputStream pipedInputStream = new PipedInputStream();
+            PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        payloadWriter.write(request, pipedOutputStream);
+                    } catch (Exception e) {
+                        Loggers.getLogger(Jdk11UnderlyingHttpExecutor.class).error("file upload failed: {}", e.getMessage(), e);
+                    } finally {
+                        IOs.close(pipedOutputStream);
+                    }
+                }
+            }).start();
+            builder.POST(HttpRequest.BodyPublishers.ofInputStream(() -> pipedInputStream));
+
+        } else {
+            builder.method(method.name(), HttpRequest.BodyPublishers.noBody());
+        }
+        builder.timeout(timeout == null ? Duration.ofSeconds(60) : timeout);
+        HttpRequest underlyingRequest = builder.build();
+
+        java.net.http.HttpResponse<byte[]> underlyingHttpResponse = null;
+        try {
+            underlyingHttpResponse = this.httpClient.send(underlyingRequest, java.net.http.HttpResponse.BodyHandlers.ofByteArray());
+            underlyingHttpResponse.statusCode();
+            java.net.http.HttpHeaders headers = underlyingHttpResponse.headers();
+            HttpHeaders responseHeaders = new HttpHeaders();
+            for (String name : headers.map().keySet()) {
+                for (String value : headers.allValues(name)) {
+                    responseHeaders.add(name, value);
+                }
+            }
+            Jdk11UnderlyingHttpResponse response = new Jdk11UnderlyingHttpResponse(request.getMethod(), request.getUri(), responseHeaders, underlyingHttpResponse.statusCode(), new ByteArrayInputStream(underlyingHttpResponse.body()));
+            return response;
+        } catch (InterruptedException e) {
+            throw Throwables.wrapAsRuntimeException(e);
+        }
+
     }
 
     @Override
@@ -80,7 +124,7 @@ public class Jdk11UnderlyingHttpExecutor extends AbstractUnderlyingHttpExecutor<
         java.net.http.HttpResponse<byte[]> underlyingHttpResponse = null;
         try {
             underlyingHttpResponse = this.httpClient.send(underlyingRequest, java.net.http.HttpResponse.BodyHandlers.ofByteArray());
-            int statusCode = underlyingHttpResponse.statusCode();
+            underlyingHttpResponse.statusCode();
             java.net.http.HttpHeaders headers = underlyingHttpResponse.headers();
             HttpHeaders responseHeaders = new HttpHeaders();
             for (String name : headers.map().keySet()) {
