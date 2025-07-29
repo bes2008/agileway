1fffb049f9994107de8f2d9ad0dbe17a2fc15751
fjn1194361820 <fs1194361820@163.com>
2025-07-29 12:03:05 +0800

    * 完善返回值类型检查

--- agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/declarative/HttpExchangeMethod.java
+++ agileway-httpclient/agileway-httpclient-core/src/main/java/com/jn/agileway/httpclient/declarative/HttpExchangeMethod.java
@@ -1,13 +1,15 @@
 package com.jn.agileway.httpclient.declarative;
 
+import com.jn.agileway.httpclient.core.HttpResponse;
 import com.jn.langx.text.StringTemplates;
-import com.jn.langx.util.Strings;
+import com.jn.langx.util.concurrent.promise.Promise;
 import com.jn.langx.util.net.http.HttpMethod;
 import com.jn.langx.util.net.mime.MediaType;
 import com.jn.langx.util.reflect.signature.TypeSignatures;
 import com.jn.langx.util.valuegetter.ArrayValueGetter;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -23,18 +25,21 @@ public class HttpExchangeMethod {
 
     private String[] accept;
 
-    private Map<String, DefaultValueSupportedValueGetter> queryParams = new LinkedHashMap<>();
+    private final Map<String, DefaultValueSupportedValueGetter> queryParams = new LinkedHashMap<>();
 
-    private Map<String, ArrayValueGetter<Object>> uriVariables = new LinkedHashMap<>();
+    private final Map<String, ArrayValueGetter<Object>> uriVariables = new LinkedHashMap<>();
 
-    private Map<String, ArrayValueGetter<Object>> headers = new LinkedHashMap<>();
+    private final Map<String, ArrayValueGetter<Object>> headers = new LinkedHashMap<>();
 
-    private Map<String, ArrayValueGetter<Object>> cookies = new LinkedHashMap<>();
+    private final Map<String, ArrayValueGetter<Object>> cookies = new LinkedHashMap<>();
 
-    private Map<String, ArrayValueGetter<Object>> bodyParts = new LinkedHashMap<>();
+    private final Map<String, ArrayValueGetter<Object>> bodyParts = new LinkedHashMap<>();
 
     private ArrayValueGetter<Object> body;
 
+    /**
+     * http response 的 content 对应的 Java类型
+     */
     private Type expectedResponseType;
 
     public String getUriTemplate() {
@@ -65,41 +70,25 @@ public class HttpExchangeMethod {
         return queryParams;
     }
 
-    public void setQueryParams(Map<String, DefaultValueSupportedValueGetter> queryParams) {
-        this.queryParams = queryParams;
-    }
 
     public Map<String, ArrayValueGetter<Object>> getUriVariables() {
         return uriVariables;
-    }
-
-    public void setUriVariables(Map<String, ArrayValueGetter<Object>> uriVariables) {
-        this.uriVariables = uriVariables;
     }
 
     public Map<String, ArrayValueGetter<Object>> getHeaders() {
         return headers;
     }
 
-    public void setHeaders(Map<String, ArrayValueGetter<Object>> headers) {
-        this.headers = headers;
-    }
 
     public Map<String, ArrayValueGetter<Object>> getCookies() {
         return cookies;
     }
 
-    public void setCookies(Map<String, ArrayValueGetter<Object>> cookies) {
-        this.cookies = cookies;
-    }
 
     public Map<String, ArrayValueGetter<Object>> getBodyParts() {
         return bodyParts;
     }
 
-    public void setBodyParts(Map<String, ArrayValueGetter<Object>> bodyParts) {
-        this.bodyParts = bodyParts;
-    }
 
     public ArrayValueGetter<Object> getBody() {
         return body;
@@ -134,17 +123,34 @@ public class HttpExchangeMethod {
     }
 
     public void checkValid() throws HttpExchangeMethodDeclaringException {
+        // java method
         if (javaMethod == null) {
-            throw new HttpExchangeMethodDeclaringException("The java method is null");
+            throw new HttpExchangeMethodDeclaringException("The java method is required");
         }
+        // http method
         if (httpMethod == null || httpMethod == HttpMethod.OPTIONS || httpMethod == HttpMethod.TRACE || httpMethod == HttpMethod.HEAD) {
             throw new HttpExchangeMethodDeclaringException(StringTemplates.formatWithPlaceholder("The http method is not valid, http exchange java method: {}", TypeSignatures.toMethodSignature(javaMethod)));
         }
-        if (Strings.isBlank(uriTemplate)) {
+        // SOAP 方法时，uri template 为 ""
+        if (uriTemplate == null) {
             throw new HttpExchangeMethodDeclaringException(StringTemplates.formatWithPlaceholder("The uriTemplate is blank, http exchange java method: {}", TypeSignatures.toMethodSignature(javaMethod)));
         }
-        if (body != null && bodyParts != null) {
+        // body 和 bodyParts 不能同时设置
+        if (body != null && !bodyParts.isEmpty()) {
             throw new HttpExchangeMethodDeclaringException(StringTemplates.formatWithPlaceholder("The body and bodyParts are both set, http exchange java method: {}", TypeSignatures.toMethodSignature(javaMethod)));
+        }
+        // 对 expectedResponseType 进行校验
+        if (expectedResponseType != null) {
+            if (expectedResponseType == Promise.class || expectedResponseType == HttpResponse.class) {
+                throw new HttpExchangeMethodDeclaringException(StringTemplates.formatWithPlaceholder("The expectedResponseType is not valid, http exchange java method: {}", TypeSignatures.toMethodSignature(javaMethod)));
+            }
+            if (expectedResponseType instanceof ParameterizedType) {
+                ParameterizedType prt = (ParameterizedType) expectedResponseType;
+                Type rawType = prt.getRawType();
+                if (rawType == Promise.class || rawType == HttpResponse.class) {
+                    throw new HttpExchangeMethodDeclaringException(StringTemplates.formatWithPlaceholder("The expectedResponseType is not valid, http exchange java method: {}", TypeSignatures.toMethodSignature(javaMethod)));
+                }
+            }
         }
     }
 }
