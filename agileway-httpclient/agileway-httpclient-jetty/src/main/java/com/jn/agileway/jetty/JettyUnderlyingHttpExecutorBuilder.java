6908f3a1cd92bde4ea565256ce546e5bb33a130b
coderjn <fs1194361820@163.com>
2025-08-08 22:06:57 +0800

    * update it

--- agileway-httpclient/agileway-httpclient-jetty/src/main/java/com/jn/agileway/jetty/JettyUnderlyingHttpExecutorBuilder.java
+++ agileway-httpclient/agileway-httpclient-jetty/src/main/java/com/jn/agileway/jetty/JettyUnderlyingHttpExecutorBuilder.java
@@ -3,6 +3,7 @@ package com.jn.agileway.jetty;
 import com.jn.agileway.httpclient.core.underlying.UnderlyingHttpExecutor;
 import com.jn.agileway.httpclient.core.underlying.UnderlyingHttpExecutorBuilder;
 import com.jn.langx.security.ssl.SSLContextBuilder;
+import com.jn.langx.util.timing.TimeDuration;
 import org.eclipse.jetty.client.HttpClient;
 
 import javax.net.ssl.HostnameVerifier;
@@ -10,6 +11,12 @@ import java.net.Proxy;
 import java.util.concurrent.ExecutorService;
 
 public class JettyUnderlyingHttpExecutorBuilder implements UnderlyingHttpExecutorBuilder {
+    private TimeDuration requestTimeout = TimeDuration.ofMinutes(2);
+    private TimeDuration connectTimeout;
+    private ExecutorService executor;
+    private SSLContextBuilder sslContextBuilder;
+    private Proxy proxy;
+
     @Override
     public UnderlyingHttpExecutorBuilder poolMaxIdleConnections(int maxIdleConnections) {
         return null;
@@ -22,17 +29,22 @@ public class JettyUnderlyingHttpExecutorBuilder implements UnderlyingHttpExecuto
 
     @Override
     public UnderlyingHttpExecutorBuilder connectTimeoutMills(int connectTimeoutInMills) {
-        return null;
+        if (connectTimeoutInMills > 0) {
+            this.connectTimeout = TimeDuration.ofMillis(connectTimeoutInMills);
+        }
+        return this;
     }
 
     @Override
     public UnderlyingHttpExecutorBuilder requestTimeoutMills(int readTimeoutInMills) {
-        return null;
+        this.requestTimeout = TimeDuration.ofMillis(readTimeoutInMills);
+        return this;
     }
 
     @Override
     public UnderlyingHttpExecutorBuilder proxy(Proxy proxy) {
-        return null;
+        this.proxy = proxy;
+        return this;
     }
 
     @Override
@@ -42,19 +54,31 @@ public class JettyUnderlyingHttpExecutorBuilder implements UnderlyingHttpExecuto
 
     @Override
     public UnderlyingHttpExecutorBuilder sslContextBuilder(SSLContextBuilder sslContextBuilder) {
-        return null;
+        this.sslContextBuilder = sslContextBuilder;
+        return this;
     }
 
     @Override
     public UnderlyingHttpExecutorBuilder executor(ExecutorService executor) {
-        return null;
+        this.executor = executor;
+        return this;
     }
 
     @Override
     public UnderlyingHttpExecutor build() {
         HttpClient httpClient = new HttpClient();
+        if (executor != null) {
+            httpClient.setExecutor(executor);
+        }
+        if (proxy != null) {
+            // httpClient.getProxyConfiguration().getProxies().add(proxy);
+        }
+        if (connectTimeout != null) {
+            httpClient.setConnectTimeout(connectTimeout.toMillis());
+        }
+        httpClient.setMaxConnectionsPerDestination(1000);
 
-        JettyUnderlyingHttpExecutor httpExecutor = new JettyUnderlyingHttpExecutor(httpClient);
+        JettyUnderlyingHttpExecutor httpExecutor = new JettyUnderlyingHttpExecutor(httpClient, requestTimeout);
         return httpExecutor;
     }
 
