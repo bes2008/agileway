da52ac81581ec12cb334ffaf68f923d4cabc0b10
coderjn <fs1194361820@163.com>
2025-08-10 12:21:22 +0800

    * update it

--- agileway-eipchannel/agileway-eipchannel-core/src/main/java/com/jn/agileway/eipchannel/core/router/DirectOutboundRouter.java
+++ agileway-eipchannel/agileway-eipchannel-core/src/main/java/com/jn/agileway/eipchannel/core/router/DirectOutboundRouter.java
@@ -3,10 +3,11 @@ package com.jn.agileway.eipchannel.core.router;
 import com.jn.agileway.eipchannel.core.channel.OutboundChannel;
 
 /**
+ * @see Point2PointMessageRouter
  * @deprecated
  */
 public class DirectOutboundRouter extends Point2PointMessageRouter {
-    public DirectOutboundRouter(OutboundChannel defaultOutboundChannel) {
-        super(defaultOutboundChannel);
+    public DirectOutboundRouter(OutboundChannel outboundChannel) {
+        super(outboundChannel);
     }
 }
