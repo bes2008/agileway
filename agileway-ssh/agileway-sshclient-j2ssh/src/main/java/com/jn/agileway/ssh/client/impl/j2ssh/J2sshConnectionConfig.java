88420ab851b9e36248c23d94a64b9380a07a88e6
fjn1194361820 <fs1194361820@163.com>
2025-08-13 10:09:21 +0800

    - 移除对j2ssh的支持模块

--- agileway-ssh/agileway-sshclient-j2ssh/src/main/java/com/jn/agileway/ssh/client/impl/j2ssh/J2sshConnectionConfig.java
+++
@@ -1,6 +0,0 @@
-package com.jn.agileway.ssh.client.impl.j2ssh;
-
-import com.jn.agileway.ssh.client.AbstractSshConnectionConfig;
-
-public class J2sshConnectionConfig extends AbstractSshConnectionConfig {
-}
