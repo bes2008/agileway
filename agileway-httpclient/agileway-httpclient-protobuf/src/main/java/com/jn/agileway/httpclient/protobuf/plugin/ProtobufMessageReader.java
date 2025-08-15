381d3d54501471a571fec543b2ead08574905250
fjn1194361820 <fs1194361820@163.com>
2025-08-15 17:11:28 +0800

    * 完善 protobuf 的 reader, writer

--- agileway-httpclient/agileway-httpclient-protobuf/src/main/java/com/jn/agileway/httpclient/protobuf/plugin/ProtobufMessageReader.java
+++ agileway-httpclient/agileway-httpclient-protobuf/src/main/java/com/jn/agileway/httpclient/protobuf/plugin/ProtobufMessageReader.java
@@ -1,19 +1,34 @@
 package com.jn.agileway.httpclient.protobuf.plugin;
 
+import com.google.protobuf.GeneratedMessage;
 import com.jn.agileway.httpclient.core.HttpResponse;
 import com.jn.agileway.httpclient.core.payload.HttpResponsePayloadReader;
 import com.jn.langx.util.net.mime.MediaType;
+import com.jn.langx.util.reflect.Reflects;
 
 import java.lang.reflect.Type;
 
-public class ProtobufMessageReader implements HttpResponsePayloadReader<Object> {
+public class ProtobufMessageReader implements HttpResponsePayloadReader<GeneratedMessage> {
     @Override
     public boolean canRead(HttpResponse<byte[]> response, MediaType contentType, Type expectedContentType) {
-        return false;
+        if (expectedContentType == null || contentType == null) {
+            return false;
+        }
+        if (!contentType.equalsTypeAndSubtype(MediaType.APPLICATION_PROTOBUF)) {
+            return false;
+        }
+        if (!(expectedContentType instanceof Class)) {
+            return false;
+        }
+        Class clazz = (Class) expectedContentType;
+        if (!Reflects.isSubClass(GeneratedMessage.class, clazz)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
-    public Object read(HttpResponse<byte[]> response, MediaType contentType, Type expectedContentType) throws Exception {
+    public GeneratedMessage read(HttpResponse<byte[]> response, MediaType contentType, Type expectedContentType) throws Exception {
         return null;
     }
 }
--- agileway-httpclient/agileway-httpclient-protobuf/src/main/java/com/jn/agileway/httpclient/protobuf/plugin/ProtobufMessageWriter.java
+++ agileway-httpclient/agileway-httpclient-protobuf/src/main/java/com/jn/agileway/httpclient/protobuf/plugin/ProtobufMessageWriter.java
@@ -1,5 +1,6 @@
 package com.jn.agileway.httpclient.protobuf.plugin;
 
+import com.google.protobuf.GeneratedMessage;
 import com.jn.agileway.httpclient.core.HttpRequest;
 import com.jn.agileway.httpclient.core.payload.HttpRequestPayloadWriter;
 
@@ -8,6 +9,9 @@ import java.io.OutputStream;
 public class ProtobufMessageWriter implements HttpRequestPayloadWriter {
     @Override
     public boolean canWrite(HttpRequest<?> request) {
+        if (request.getPayload() instanceof GeneratedMessage) {
+            return true;
+        }
         return false;
     }
 
