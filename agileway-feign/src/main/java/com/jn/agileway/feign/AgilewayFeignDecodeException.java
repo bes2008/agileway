1507fc596018a4fd540803d1c22556e8d2b85624
fjn1194361820 <fs1194361820@163.com>
2025-08-26 12:00:56 +0800

    - 移除对 feign的支持

--- agileway-feign/src/main/java/com/jn/agileway/feign/AgilewayFeignDecodeException.java
+++
@@ -1,9 +0,0 @@
-package com.jn.agileway.feign;
-
-import feign.FeignException;
-
-public class AgilewayFeignDecodeException extends FeignException {
-    public AgilewayFeignDecodeException(int status, String message) {
-        super( status, message);
-    }
-}
