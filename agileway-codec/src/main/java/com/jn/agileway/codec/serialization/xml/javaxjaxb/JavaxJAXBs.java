e0e50e7e0d680fdda77aa1d4a626b3d8836b17f0
coderjn <fs1194361820@163.com>
2025-09-06 10:11:21 +0800

    * update it

--- agileway-codec/src/main/java/com/jn/agileway/codec/serialization/xml/javaxjaxb/JavaxJAXBs.java
+++ agileway-codec/src/main/java/com/jn/agileway/codec/serialization/xml/javaxjaxb/JavaxJAXBs.java
@@ -9,31 +9,36 @@ import javax.xml.bind.Unmarshaller;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class JavaxJAXBs {
     private JavaxJAXBs() {
     }
 
-    public static byte[] serialize(Object javaBean) throws Exception {
+    public static byte[] marshal(Object javaBean) throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        marshal(javaBean, outputStream);
+        return outputStream.toByteArray();
+    }
+
+    public static void marshal(Object javaBean, OutputStream out) throws Exception {
         JAXBContext jaxbContext = JAXBContext.newInstance(javaBean.getClass());
         Marshaller marshaller = jaxbContext.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
         // 自动换行、缩进
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        marshaller.marshal(javaBean, outputStream);
-        return outputStream.toByteArray();
+        marshaller.marshal(javaBean, out);
     }
 
-    public static <T> T deserialize(String xml, Class<T> expectedClazz) throws Exception {
+    public static <T> T unmarshal(String xml, Class<T> expectedClazz) throws Exception {
         if (Strings.isBlank(xml)) {
             return null;
         }
-        return deserialize(new ByteArrayInputStream(xml.getBytes(Charsets.UTF_8)), expectedClazz);
+        return unmarshal(new ByteArrayInputStream(xml.getBytes(Charsets.UTF_8)), expectedClazz);
     }
 
-    public static <T> T deserialize(InputStream xml, Class<T> expectedClazz) throws Exception {
+    public static <T> T unmarshal(InputStream xml, Class<T> expectedClazz) throws Exception {
         JAXBContext jaxbContext = JAXBContext.newInstance(expectedClazz);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
         return (T) unmarshaller.unmarshal(xml);
